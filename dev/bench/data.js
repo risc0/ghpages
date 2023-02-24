dda9b652c451f5e1f3397306d2f525e9bf0a8bf5
github-action-benchmark <github@users.noreply.github.com>
2023-02-24 19:57:38 +0000

    add Linux-nvidia_rtx_a5000 (cargo) benchmark result for 0e9b54d3398ad7dcda761a08d8162142b7d29f07

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677268646768,
+  "lastUpdate": 1677268658723,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-intel_uhd_630": [
@@ -89,6 +89,52 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 1062913807,
             "range": "± 4770931",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "Linux-nvidia_rtx_a5000": [
+      {
+        "commit": {
+          "author": {
+            "name": "risc0",
+            "username": "risc0"
+          },
+          "committer": {
+            "name": "risc0",
+            "username": "risc0"
+          },
+          "id": "0e9b54d3398ad7dcda761a08d8162142b7d29f07",
+          "message": "Benchmark workflows",
+          "timestamp": "2023-02-24T19:43:22Z",
+          "url": "https://github.com/risc0/risc0/pull/398/commits/0e9b54d3398ad7dcda761a08d8162142b7d29f07"
+        },
+        "date": 1677268657858,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 849934198,
+            "range": "± 8532155",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 847133057,
+            "range": "± 5965451",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 470189269,
+            "range": "± 3154242",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 475628833,
+            "range": "± 4749081",
             "unit": "ns/iter"
           }
         ]
