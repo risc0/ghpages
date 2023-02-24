f416a976855fc1eb2c6f75ea90f6017e5eb9cc53
github-action-benchmark <github@users.noreply.github.com>
2023-02-24 23:55:30 +0000

    add Linux-nvidia_rtx_a5000 (cargo) benchmark result for d6e4d9123334a6ba4bf15069afb393d766067c7f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677269144784,
+  "lastUpdate": 1677282930905,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-intel_uhd_630": [
@@ -135,6 +135,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 475628833,
             "range": "± 4749081",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "parker@risczero.com",
+            "name": "Parker Thompson",
+            "username": "mothran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d6e4d9123334a6ba4bf15069afb393d766067c7f",
+          "message": "Benchmark workflows (#398)\n\n* Added first rev of benchmark workflow\r\n\r\n* Fixed matrix.arch typo\r\n\r\n* Update repo name\r\n\r\n* Drops token PERMS and moved gh-action-bench to v1\r\n\r\n* Broke up benchmarks into two workflows\r\n\r\nCreated a Criterion compare workflow for PRs and kept the github pages\r\nworkflow for all pushes to main\r\n\r\n* Added required branchName to workflow\r\n\r\n* corrected benchmark name\r\n\r\n* test change\r\n\r\n* Fixed trailing }\r\n\r\n* Added PR trigger for testing\r\n\r\n* Removed the PR trigger for bench_trends\r\n\r\n* Test commit - testing PR check workflow updates",
+          "timestamp": "2023-02-24T15:41:46-08:00",
+          "tree_id": "d5a73b982877112e322f71ff92f14573d32d9c9f",
+          "url": "https://github.com/risc0/risc0/commit/d6e4d9123334a6ba4bf15069afb393d766067c7f"
+        },
+        "date": 1677282930017,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 850045553,
+            "range": "± 5111417",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 847868956,
+            "range": "± 10299421",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 471366536,
+            "range": "± 4776066",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 474798782,
+            "range": "± 4781342",
             "unit": "ns/iter"
           }
         ]
