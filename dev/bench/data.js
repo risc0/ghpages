a39427e010f7b322f7bd041198d5f09de34605a5
github-action-benchmark <github@users.noreply.github.com>
2023-02-27 19:48:52 +0000

    add Linux-nvidia_rtx_a5000 (cargo) benchmark result for adfea4309cd0a1fa8e7b9772bcd9ce501bebd195

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677520144681,
+  "lastUpdate": 1677527332048,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-intel_uhd_630": [
@@ -567,6 +567,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 470685781,
             "range": "± 3581570",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "adfea4309cd0a1fa8e7b9772bcd9ce501bebd195",
+          "message": "Restrict benchmarks to bench machines (#404)",
+          "timestamp": "2023-02-27T19:35:32Z",
+          "tree_id": "5414713b64927a80645a780f4be20595ee903622",
+          "url": "https://github.com/risc0/risc0/commit/adfea4309cd0a1fa8e7b9772bcd9ce501bebd195"
+        },
+        "date": 1677527331143,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 849685361,
+            "range": "± 6246800",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 858889559,
+            "range": "± 7568452",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 469658253,
+            "range": "± 4157642",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 477625676,
+            "range": "± 4758843",
             "unit": "ns/iter"
           }
         ]
