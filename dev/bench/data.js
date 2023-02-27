3c3c9bf9253994ab15e89a1da6e4add39f640df1
github-action-benchmark <github@users.noreply.github.com>
2023-02-27 15:48:41 -0800

    add macOS-cpu (cargo) benchmark result for e680f8df584ca5f971cff3e1cc2bec577739b777

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1677541713595,
+  "lastUpdate": 1677541721813,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-intel_uhd_630": [
@@ -617,6 +617,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 1324504471,
             "range": "± 6541762",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e680f8df584ca5f971cff3e1cc2bec577739b777",
+          "message": "Drop vulnerable remove_dir_all dep (#405)",
+          "timestamp": "2023-02-27T23:32:10Z",
+          "tree_id": "ad15f4810a304baf91f0e6e6c955a850727dbeb4",
+          "url": "https://github.com/risc0/risc0/commit/e680f8df584ca5f971cff3e1cc2bec577739b777"
+        },
+        "date": 1677541720721,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 4274387790,
+            "range": "± 47270329",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 4306901057,
+            "range": "± 80406957",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 1072488557,
+            "range": "± 5649175",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 1070901373,
+            "range": "± 3589585",
             "unit": "ns/iter"
           }
         ]
