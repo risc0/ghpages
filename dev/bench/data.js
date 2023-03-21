73fa48ad871641295b37d0ead11efef4c87b7506
github-action-benchmark <github@users.noreply.github.com>
2023-03-21 21:19:45 +0000

    add Linux-nvidia_rtx_a5000 (cargo) benchmark result for fc27559ad8d5f4a35712256ca38b94b394249d6d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679433020025,
+  "lastUpdate": 1679433585405,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -3593,6 +3593,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 629585946,
             "range": "± 8632633",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "parker@risczero.com",
+            "name": "Parker Thompson",
+            "username": "mothran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fc27559ad8d5f4a35712256ca38b94b394249d6d",
+          "message": "Move benchmark comment files to runner.temp (#462)\n\nMoves all benchmark file artifacts to runner.temp to prevent files from being read cross-workflow or cross-job",
+          "timestamp": "2023-03-21T14:04:44-07:00",
+          "tree_id": "205c43e051ac8c0be7b43603509fe0820cb29be4",
+          "url": "https://github.com/risc0/risc0/commit/fc27559ad8d5f4a35712256ca38b94b394249d6d"
+        },
+        "date": 1679433584396,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 873988868,
+            "range": "± 5661808",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 874426245,
+            "range": "± 8960928",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 500420404,
+            "range": "± 5565430",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 499385686,
+            "range": "± 7041361",
             "unit": "ns/iter"
           }
         ]
