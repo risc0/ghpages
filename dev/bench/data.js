6484a967d455bb40f546a57f345a9e7112656bf0
github-action-benchmark <github@users.noreply.github.com>
2023-03-20 17:30:28 -0700

    add macOS-cpu (cargo) benchmark result for e7b19d57877577a2735e69172ea3e445a2c0c6cb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1679358605826,
+  "lastUpdate": 1679358628955,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -1675,6 +1675,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 778838666,
             "range": "± 1749057",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "parker@risczero.com",
+            "name": "Parker Thompson",
+            "username": "mothran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e7b19d57877577a2735e69172ea3e445a2c0c6cb",
+          "message": "Adjusted benchmark workflow with PR number (#458)\n\nAdded PR number into artifact for workflow_run to pickup and correctly create a PR comment.",
+          "timestamp": "2023-03-20T17:27:18-07:00",
+          "tree_id": "4464642f24228823892daedbe0471f41ab367fbe",
+          "url": "https://github.com/risc0/risc0/commit/e7b19d57877577a2735e69172ea3e445a2c0c6cb"
+        },
+        "date": 1679358627680,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 2165733124,
+            "range": "± 21413717",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 2165503645,
+            "range": "± 28364102",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 778010812,
+            "range": "± 951044",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 779183250,
+            "range": "± 850772",
             "unit": "ns/iter"
           }
         ]
