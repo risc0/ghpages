b43f4ccd8761516cbeefe95850f0f77d193730f2
github-action-benchmark <github@users.noreply.github.com>
2023-04-04 23:22:18 +0000

    add Linux-nvidia_rtx_a5000 (cargo) benchmark result for b54a0b3b9a9f67da6263b9892c418db0f010a8a1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1680650516907,
+  "lastUpdate": 1680650538184,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -5273,6 +5273,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 605172957,
             "range": "± 2539789",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "parker@risczero.com",
+            "name": "Parker Thompson",
+            "username": "mothran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b54a0b3b9a9f67da6263b9892c418db0f010a8a1",
+          "message": "Moved tempdir for risc0-build into cache_dir (#497)",
+          "timestamp": "2023-04-04T23:19:20Z",
+          "tree_id": "f9d924e52ddc41a3e6a77e2bf99125dc2022e055",
+          "url": "https://github.com/risc0/risc0/commit/b54a0b3b9a9f67da6263b9892c418db0f010a8a1"
+        },
+        "date": 1680650537229,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 722412617,
+            "range": "± 12700459",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 729676511,
+            "range": "± 21922511",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 311254922,
+            "range": "± 6145688",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 311933837,
+            "range": "± 5204083",
             "unit": "ns/iter"
           }
         ]
