53c76996d9e337ef41459f69d0c2fa6f49aa25a6
github-action-benchmark <github@users.noreply.github.com>
2023-04-17 18:47:02 +0000

    add Linux-cpu (cargo) benchmark result for 08b89140bbf1b28f87b62cbc2cdac8e9820af72b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681756765723,
+  "lastUpdate": 1681757222011,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -9303,6 +9303,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 340670824,
             "range": "± 3993942",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "08b89140bbf1b28f87b62cbc2cdac8e9820af72b",
+          "message": "Implementation of new API, part 1 (#503)",
+          "timestamp": "2023-04-17T11:35:00-07:00",
+          "tree_id": "1f46cbe5852d43fd771be3905002158f88e65216",
+          "url": "https://github.com/risc0/risc0/commit/08b89140bbf1b28f87b62cbc2cdac8e9820af72b"
+        },
+        "date": 1681757220825,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 2305829718,
+            "range": "± 16153268",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 2310249059,
+            "range": "± 15009021",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 323918628,
+            "range": "± 4196900",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 336009003,
+            "range": "± 5751751",
             "unit": "ns/iter"
           }
         ]
