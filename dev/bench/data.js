3fea022c598dca712f395452f0ed05de8fa7b82a
github-action-benchmark <github@users.noreply.github.com>
2023-04-18 15:39:26 +0000

    add Linux-cpu (cargo) benchmark result for bdb811ab9a3f43f7cb589bfeb25df9aebef95f6f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681831921339,
+  "lastUpdate": 1681832366794,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -9591,6 +9591,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 333330901,
             "range": "± 6011790",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "parker@risczero.com",
+            "name": "Parker Thompson",
+            "username": "mothran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bdb811ab9a3f43f7cb589bfeb25df9aebef95f6f",
+          "message": "Add additional sanity checking to the ELF parser (#515)\n\n* Add bounds checking for elf parser.",
+          "timestamp": "2023-04-18T08:27:20-07:00",
+          "tree_id": "c63a2cdbb402d7e57e127349c5303cb322bd7e6b",
+          "url": "https://github.com/risc0/risc0/commit/bdb811ab9a3f43f7cb589bfeb25df9aebef95f6f"
+        },
+        "date": 1681832365658,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 2330884380,
+            "range": "± 17984219",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 2324989012,
+            "range": "± 15780431",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 330758277,
+            "range": "± 4276671",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 334830330,
+            "range": "± 4362125",
             "unit": "ns/iter"
           }
         ]
