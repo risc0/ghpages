a45f2bbed6ef198d2196bae66a2ec16bd45a3b89
github-action-benchmark <github@users.noreply.github.com>
2023-04-18 20:52:24 +0000

    add Linux-cpu (cargo) benchmark result for 909214982650666c03159e9bad9d2ee90d5e2a46

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1681851101819,
+  "lastUpdate": 1681851144449,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -9735,6 +9735,54 @@ window.BENCHMARK_DATA = {
             "name": "fib/200/run",
             "value": 334830330,
             "range": "± 4362125",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "parker@risczero.com",
+            "name": "Parker Thompson",
+            "username": "mothran"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "909214982650666c03159e9bad9d2ee90d5e2a46",
+          "message": "Added templ_subdir flag to cargo risczero new (#517)\n\n* Adds additional flags to cargo risczero new for sub directory selection\r\n---------\r\n\r\nCo-authored-by: Tim Zerrell <tim.zerrell@risczero.com>",
+          "timestamp": "2023-04-18T13:47:26-07:00",
+          "tree_id": "0eddae9f17a96b6724120e63f89bbc91caf14e8d",
+          "url": "https://github.com/risc0/risc0/commit/909214982650666c03159e9bad9d2ee90d5e2a46"
+        },
+        "date": 1681851143405,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/proof",
+            "value": 2301171843,
+            "range": "± 17438246",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/proof",
+            "value": 2292409535,
+            "range": "± 20466035",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/run",
+            "value": 322764020,
+            "range": "± 7130407",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/200/run",
+            "value": 325840234,
+            "range": "± 1655080",
             "unit": "ns/iter"
           }
         ]
