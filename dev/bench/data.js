98eef6c463fed28c98590d42ade5dd1003007d2b
github-action-benchmark <github@users.noreply.github.com>
2023-06-13 05:31:13 +0000

    add Linux-cpu (cargo) benchmark result for f448fbd730b9aba8fab79ba6fd67549caddb85cf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1686633582459,
+  "lastUpdate": 1686634273137,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -19179,6 +19179,84 @@ window.BENCHMARK_DATA = {
             "name": "fib/10000/total",
             "value": 21399060929,
             "range": "± 126234198",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f448fbd730b9aba8fab79ba6fd67549caddb85cf",
+          "message": "implement SessionReceipt trait (#616)\n\nThis pull request implements the `SessionReceipt` trait that is implemented by\r\n`SessionRollupReceipt` and `SessionFlatReceipt` (also known as the old\r\n`SessionReceipt`). This is useful because it allows the host code to call verify\r\nand to get the journal contents regardless of the concrete type of session\r\nreceipt. This trait is practical when a developer has a code base that uses\r\nbonsai resulting in a `SessionRollupReceipt` or from a local prover resulting in a\r\n`SessionFlatReceipt`.  Examples have been updated to use this new trait. The main\r\ndifference is to replace existing `receipt.journal` with `receipt.get_journal()`\r\nand `receipt.verify(XXXX_ID)` with `receipt.verify(XXXX_ID.into())`.",
+          "timestamp": "2023-06-13T05:05:10Z",
+          "tree_id": "b2caadff7b30c021a2ee2931744b0ede22f077cd",
+          "url": "https://github.com/risc0/risc0/commit/f448fbd730b9aba8fab79ba6fd67549caddb85cf"
+        },
+        "date": 1686634271807,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/execute",
+            "value": 55108282,
+            "range": "± 537617",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/execute",
+            "value": 67849585,
+            "range": "± 572313",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/execute",
+            "value": 195141141,
+            "range": "± 1682217",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/prove",
+            "value": 5061610455,
+            "range": "± 27567450",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/prove",
+            "value": 5100423589,
+            "range": "± 14673372",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/prove",
+            "value": 21092655915,
+            "range": "± 65847083",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/total",
+            "value": 5124146562,
+            "range": "± 32308333",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/total",
+            "value": 5183152805,
+            "range": "± 29993324",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/total",
+            "value": 21267090423,
+            "range": "± 87092614",
             "unit": "ns/iter"
           }
         ]
