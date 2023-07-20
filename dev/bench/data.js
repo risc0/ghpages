57465d29850cf17cfcae0e1fed35f565c291df64
github-action-benchmark <github@users.noreply.github.com>
2023-07-19 23:24:54 -0700

    add macOS-apple_m2_pro (cargo) benchmark result for b7837861ccc629994876076e1f3936e348514764

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1689820604816,
+  "lastUpdate": 1689834294471,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -32045,6 +32045,84 @@ window.BENCHMARK_DATA = {
             "name": "fib/10000/total",
             "value": 3316929063,
             "range": "± 9925961",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b7837861ccc629994876076e1f3936e348514764",
+          "message": "zkVM: split binfmt feature to a new \"risc0-binfmt\" crate for modularity (#725)\n\n* Add a new package risc0-binfmt\r\n\r\nWe need to drop risc0-zkvm as a dependency for risc0-build to avoid future\r\ncircular dependencies. In order to do so, we introduce a new crate,\r\nrisc0-binfmt that contains all code needed by risc0-build that used to be a\r\npart of risc0-zkvm. This crate contains Program, MemoryImage, and SystemState\r\nstruct definitions along with several functions that operate on it.\r\n\r\n* drop binfmt feature and use risc0-binfmt instead\r\n\r\nThis change removes files that implement MemoryImage, Program, SystemState, and\r\nthe binfmt feature of risc0-zkvm. Instead, it uses the new risc0-binfmt crate.",
+          "timestamp": "2023-07-20T06:15:37Z",
+          "tree_id": "fd7b8857552ef965c3fd2eae40ba548390b01f78",
+          "url": "https://github.com/risc0/risc0/commit/b7837861ccc629994876076e1f3936e348514764"
+        },
+        "date": 1689834292841,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/execute",
+            "value": 2674384,
+            "range": "± 100046",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/execute",
+            "value": 3039197,
+            "range": "± 121902",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/execute",
+            "value": 5106112,
+            "range": "± 123495",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/prove",
+            "value": 853548938,
+            "range": "± 4541370",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/prove",
+            "value": 872006500,
+            "range": "± 3637471",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/prove",
+            "value": 3336134604,
+            "range": "± 19461398",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/total",
+            "value": 881578729,
+            "range": "± 7036411",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/total",
+            "value": 896041479,
+            "range": "± 4190448",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/total",
+            "value": 3335649458,
+            "range": "± 7513173",
             "unit": "ns/iter"
           }
         ]
