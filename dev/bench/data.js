bf86a6f8b0d3e47bb09741bd22b97299d945c391
github-action-benchmark <github@users.noreply.github.com>
2023-08-08 17:57:44 -0700

    add macOS-apple_m2_pro (cargo) benchmark result for acb4c9e605d1eb39096c2f54e7ee4fe0aa0b9d54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691108737083,
+  "lastUpdate": 1691542664817,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -35009,6 +35009,84 @@ window.BENCHMARK_DATA = {
             "name": "fib/10000/total",
             "value": 3340939666,
             "range": "± 11150095",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor@risczero.com",
+            "name": "Victor Graf",
+            "username": "nategraf"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "acb4c9e605d1eb39096c2f54e7ee4fe0aa0b9d54",
+          "message": "Support SNARK seals in Bonsai relay and enable CI for Solidity code via Foundry (#712)\n\n    * Adds support for supplying SNARKs to authenticate callbacks through the BonsaiRelay contract, thus enabling the fully-verifying Bonsai callback flow for use in local deployment and testnet environments such as Sepolia.\r\n    * Updates the Governance showcase to be based on the latest version of the Foundry template, including supporting the run and upload subcommands in the local relay CLI.\r\n    * Adds GitHub actions workflows for testing and linting Solidity code with Foundry. It includes actions to trigger forge fmt and forge test in bonsai/ethereum (the Solidity library for Bonsai) and bonsai/examples/governance (Governance showcase).",
+          "timestamp": "2023-08-08T17:40:12-07:00",
+          "tree_id": "9839db4f2f9bd32f9621352b217a5c3d03088e01",
+          "url": "https://github.com/risc0/risc0/commit/acb4c9e605d1eb39096c2f54e7ee4fe0aa0b9d54"
+        },
+        "date": 1691542662745,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/execute",
+            "value": 2737010,
+            "range": "± 144088",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/execute",
+            "value": 3015711,
+            "range": "± 34990",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/execute",
+            "value": 5137317,
+            "range": "± 66616",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/prove",
+            "value": 853518020,
+            "range": "± 6848888",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/prove",
+            "value": 876373979,
+            "range": "± 5095125",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/prove",
+            "value": 3348649417,
+            "range": "± 11100682",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/total",
+            "value": 878507520,
+            "range": "± 7428967",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/total",
+            "value": 902369146,
+            "range": "± 7690895",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/total",
+            "value": 3347813500,
+            "range": "± 11390310",
             "unit": "ns/iter"
           }
         ]
