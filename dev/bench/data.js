d91d8ab15b556d084b1727d0d7c1b146008c6b44
github-action-benchmark <github@users.noreply.github.com>
2023-08-10 02:58:38 +0000

    add Linux-nvidia_rtx_a5000 (cargo) benchmark result for 85e40b01f568a646e02ca57f4aaeb05e5b3857c4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1691634859893,
+  "lastUpdate": 1691636318439,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -18047,6 +18047,84 @@ window.BENCHMARK_DATA = {
             "name": "fib/10000/total",
             "value": 2701804832,
             "range": "± 1834129",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "85e40b01f568a646e02ca57f4aaeb05e5b3857c4",
+          "message": "Add cargo-risczero install (#773)\n\nUse the `cargo-risczero` tool to install the `risv32im-risc0-zkvm-elf` toolchain to avoid the use of the unstable `-Z build-std` cargo feature. This fixes #754.\r\n\r\nThis change means that a rustup toolchain named `risc0` must be installed for `risc0-build` to work. To make installing this toolchain easier, the `cargo-risczero` tool has been extended to have the following commands:\r\n\r\n* `install`: This will download the latest rust toolchain published as a release in https://github.com/risc0/rust/releases.  It will then use `rustup toolchain link risc0 $SYSROOT` to make the toolchain available to rustup. `risc0-build` will run `cargo +risc0 build ...` in order to select this new toolchain.\r\n* `build-toolchain`: This command will download the latest source of the rust fork and build it locally. It will then install it using `rustup toolchain link`. This command is useful for building and publishing a new release of the fork. It can also be used to build the toolchain on a host that we don't have a prebuild archive published.",
+          "timestamp": "2023-08-10T09:05:15+07:00",
+          "tree_id": "11e7dab2ae9cc9c83f63cd0ff3899b1773829cd7",
+          "url": "https://github.com/risc0/risc0/commit/85e40b01f568a646e02ca57f4aaeb05e5b3857c4"
+        },
+        "date": 1691636316581,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/execute",
+            "value": 5140178,
+            "range": "± 101169",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/execute",
+            "value": 5587617,
+            "range": "± 114621",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/execute",
+            "value": 10309263,
+            "range": "± 140105",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/prove",
+            "value": 1257132201,
+            "range": "± 15478833",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/prove",
+            "value": 1284611757,
+            "range": "± 11980606",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/prove",
+            "value": 3856557518,
+            "range": "± 15679385",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/total",
+            "value": 1257231024,
+            "range": "± 11134761",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/total",
+            "value": 1292719544,
+            "range": "± 20413582",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/total",
+            "value": 3879188026,
+            "range": "± 17519368",
             "unit": "ns/iter"
           }
         ]
