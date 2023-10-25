d5065419bef232254d002370f26393aee07f0eac
github-action-benchmark <github@users.noreply.github.com>
2023-10-25 18:16:12 +0000

    add Linux-cpu (cargo) benchmark result for 46f0d27dd3fcd4f8cbfda0a5b8c5d0f1ede2c332

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1698257640502,
+  "lastUpdate": 1698257772571,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -33153,6 +33153,84 @@ window.BENCHMARK_DATA = {
             "name": "fib/10000/total",
             "value": 38955621438,
             "range": "± 95534888",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "46f0d27dd3fcd4f8cbfda0a5b8c5d0f1ede2c332",
+          "message": " cargo-risczero new: add --guest-name option (#1032)\n\nThe guest name parameter is used as the name of the guest package\r\nas well as method ID and ELF constants in the guest code. Therefore,\r\nthis\r\nparameter is required to be a valid rust identifier. If this is not set,\r\nthe\r\n`cargo risczero new` command will prompt the user to enter a guest name.\r\nIf no value is supplied by the command line parameter or the prompt, the\r\ndefault value for this parameter will be \"method\".\r\n\r\ncloses: https://github.com/risc0/scratch/issues/102\r\ncloses: #470\r\n\r\n---------\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2023-10-25T17:33:12Z",
+          "tree_id": "0556162a9fd26cc2dab71b9923b63e9388d551ac",
+          "url": "https://github.com/risc0/risc0/commit/46f0d27dd3fcd4f8cbfda0a5b8c5d0f1ede2c332"
+        },
+        "date": 1698257770298,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/execute",
+            "value": 25913429,
+            "range": "± 437297",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/execute",
+            "value": 27129586,
+            "range": "± 673622",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/execute",
+            "value": 33896272,
+            "range": "± 437612",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/prove",
+            "value": 9647707324,
+            "range": "± 43445542",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/prove",
+            "value": 9667542851,
+            "range": "± 29068438",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/prove",
+            "value": 38862828644,
+            "range": "± 113350432",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/total",
+            "value": 9627108389,
+            "range": "± 36172098",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/total",
+            "value": 9692822577,
+            "range": "± 31420709",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/total",
+            "value": 38847553047,
+            "range": "± 203681986",
             "unit": "ns/iter"
           }
         ]
