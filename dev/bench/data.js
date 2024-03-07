9d080dde389893f33e9106f6266571f4e8cbd41b
github-action-benchmark <github@users.noreply.github.com>
2024-03-07 21:59:24 +0000

    add Linux-nvidia_rtx_a5000 (cargo) benchmark result for 55dc5108b8d63c71fb417ac53e8149d48b20f059

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709848715311,
+  "lastUpdate": 1709848764196,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -39001,6 +39001,84 @@ window.BENCHMARK_DATA = {
             "name": "fib/10000/total",
             "value": 2266788922,
             "range": "± 8421931",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "55dc5108b8d63c71fb417ac53e8149d48b20f059",
+          "message": "Revert \"risc0-build: move the env var check for RISC0_SKIP_BUILD (#1419)\" (#1521)\n\nThis reverts commit c68575c3bea0f2bcf275dbc60d99fc59dd73dbdd and moves\r\nthe logic to skip build inside docker and non-docker build functions.\r\n\r\nMoving the env var to the embed_methods results in skipping the\r\nembedding of methods in addition to skipping the build. By doing this,\r\nRISC0_SKIP_BUILD will only skip the build and continue to embed the\r\nmethods.rs file containing the ELF and image ID.",
+          "timestamp": "2024-03-07T21:53:18Z",
+          "tree_id": "a991eff1b4edab995f9b67857caa6c90ef1edb98",
+          "url": "https://github.com/risc0/risc0/commit/55dc5108b8d63c71fb417ac53e8149d48b20f059"
+        },
+        "date": 1709848758786,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "fib/100/execute",
+            "value": 38841906,
+            "range": "± 81461",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/execute",
+            "value": 39289627,
+            "range": "± 117488",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/execute",
+            "value": 45333124,
+            "range": "± 67414",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/prove",
+            "value": 831028192,
+            "range": "± 15173821",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/prove",
+            "value": 847180561,
+            "range": "± 15375164",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/prove",
+            "value": 2651385238,
+            "range": "± 12643921",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/100/total",
+            "value": 863130533,
+            "range": "± 7611915",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/1000/total",
+            "value": 892352614,
+            "range": "± 10894160",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib/10000/total",
+            "value": 2765630980,
+            "range": "± 12254555",
             "unit": "ns/iter"
           }
         ]
