533a7ad4c847679747e3361c7a4118e9abef3b70
github-action-benchmark <github@users.noreply.github.com>
2024-04-01 22:17:06 -0700

    add macOS-cpu (customBiggerIsBetter) benchmark result for 599cfacbe9d52921466314e3ba9b75b0a99473ba

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712034970263,
+  "lastUpdate": 1712035026234,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -20652,6 +20652,61 @@ window.BENCHMARK_DATA = {
             "value": 68282382625,
             "range": "± 4727328370",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "risc0",
+            "username": "risc0"
+          },
+          "committer": {
+            "name": "risc0",
+            "username": "risc0"
+          },
+          "id": "599cfacbe9d52921466314e3ba9b75b0a99473ba",
+          "message": "Improved benchmarking",
+          "timestamp": "2024-04-01T08:04:04Z",
+          "url": "https://github.com/risc0/risc0/pull/1621/commits/599cfacbe9d52921466314e3ba9b75b0a99473ba"
+        },
+        "date": 1712035023410,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 16934188,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 27174,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 18467,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 32095,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 31527,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 27235,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 16089,
+            "unit": "Hz"
           }
         ]
       }
