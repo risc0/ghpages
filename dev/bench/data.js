295a53b2a3ebceebdc1069cc3ae0547e69623744
github-action-benchmark <github@users.noreply.github.com>
2024-04-15 21:55:05 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 46e31763e18937d79764dafd3ac2f26ed941b825

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713204966506,
+  "lastUpdate": 1713218104895,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -61745,6 +61745,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 19483,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "46e31763e18937d79764dafd3ac2f26ed941b825",
+          "message": "Include flags for caching of GPU kernels (#1672)\n\nCo-authored-by: morgangibbs87 <133707571+morgangibbs87@users.noreply.github.com>",
+          "timestamp": "2024-04-15T21:48:25Z",
+          "tree_id": "75912d618649a6b086c051dcf70480730197f4e9",
+          "url": "https://github.com/risc0/risc0/commit/46e31763e18937d79764dafd3ac2f26ed941b825"
+        },
+        "date": 1713218100006,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 10319897,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 33749,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 22826,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 42098,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 41031,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 33810,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 20156,
             "unit": "Hz"
           }
         ]
