a1f19ae4e67e7362f6c5fee1717f91660f833c61
github-action-benchmark <github@users.noreply.github.com>
2024-04-17 16:53:16 -0700

    add macOS-cpu (customBiggerIsBetter) benchmark result for ae02cad19576f056c39cc3a40e9177ae6328e031

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713397729579,
+  "lastUpdate": 1713397996815,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -21414,6 +21414,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 16003,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tim.zerrell@risczero.com",
+            "name": "Tim Zerrell",
+            "username": "tzerrell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ae02cad19576f056c39cc3a40e9177ae6328e031",
+          "message": "Require Stable Circom (#1675)\n\n---------\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-04-17T23:33:20Z",
+          "tree_id": "e10650a361ae49cc08f7b98ca92917110d020532",
+          "url": "https://github.com/risc0/risc0/commit/ae02cad19576f056c39cc3a40e9177ae6328e031"
+        },
+        "date": 1713397994603,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 16025134,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 27223,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 18384,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 31920,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 31399,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 26827,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 16084,
             "unit": "Hz"
           }
         ]
