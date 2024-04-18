648bcd41d9004ef43a06e7e5120f2ac4824027ff
github-action-benchmark <github@users.noreply.github.com>
2024-04-18 05:06:31 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 461c57b8bc1e47d71847832f08836755b37d2dbd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713416596837,
+  "lastUpdate": 1713416791012,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -85311,6 +85311,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 191323,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "461c57b8bc1e47d71847832f08836755b37d2dbd",
+          "message": "Fixing many typos (#1683)\n\nSupersedes #1319\r\n\r\n---------\r\n\r\nCo-authored-by: weikengchen <w.k@berkeley.edu>",
+          "timestamp": "2024-04-17T21:58:38-07:00",
+          "tree_id": "8e71ed76d5f3e644cdaf042fdfd0e85f47819893",
+          "url": "https://github.com/risc0/risc0/commit/461c57b8bc1e47d71847832f08836755b37d2dbd"
+        },
+        "date": 1713416786767,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 10680708,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 232512,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 231239,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 372908,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 292314,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 227213,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 192745,
             "unit": "Hz"
           }
         ]
