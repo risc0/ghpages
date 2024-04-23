9849907d580236bdae9b362ccd389bfbd995b2aa
github-action-benchmark <github@users.noreply.github.com>
2024-04-23 15:04:52 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 440caf149bf97448a509ce43d5eb00d247266ac8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713909244797,
+  "lastUpdate": 1713909892189,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -85120,6 +85120,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 68250,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "440caf149bf97448a509ce43d5eb00d247266ac8",
+          "message": "CI: Update guest docker image (#1708)\n\nThis PR updates the guest docker images to use the updated 1.77.2\r\ntoolchain. Once this PR lands, I will mark this prerelease as the\r\nhttps://github.com/risc0/rust/releases/tag/v2024-04-22.0 as the latest\r\nrelease",
+          "timestamp": "2024-04-23T21:45:07Z",
+          "tree_id": "805f1a5a5d31d1299cf856f1d8e2fef799d49247",
+          "url": "https://github.com/risc0/risc0/commit/440caf149bf97448a509ce43d5eb00d247266ac8"
+        },
+        "date": 1713909889888,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 15704112,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 86696,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 77299,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 154335,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 140583,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 85847,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 68072,
             "unit": "Hz"
           }
         ]
