9a5196d35e58a593cafc0d3244970fc0d83ba7f5
github-action-benchmark <github@users.noreply.github.com>
2024-04-24 12:16:21 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for f7a45909db26b6a0df61e8cba107bee7102cf2bc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713944923712,
+  "lastUpdate": 1713986181222,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -86300,6 +86300,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 68229,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "62482583+CobraLin@users.noreply.github.com",
+            "name": "Cobralin",
+            "username": "CobraLin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f7a45909db26b6a0df61e8cba107bee7102cf2bc",
+          "message": "Fix typo (#1691)\n\nfix some minor typo\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>\r\nCo-authored-by: Erik Kaneda <erik@risczero.com>",
+          "timestamp": "2024-04-24T12:06:51-07:00",
+          "tree_id": "14605e74e70ecada4be9265fd24b6d9d063bb0ba",
+          "url": "https://github.com/risc0/risc0/commit/f7a45909db26b6a0df61e8cba107bee7102cf2bc"
+        },
+        "date": 1713986178936,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 15672099,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 86886,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 76871,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 154396,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 142155,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 85434,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 68100,
             "unit": "Hz"
           }
         ]
