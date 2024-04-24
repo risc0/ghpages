75a837fd8e2eaa387c529e3c5627013fbef2e168
github-action-benchmark <github@users.noreply.github.com>
2024-04-23 19:24:25 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 90961af6a4b9e19872e1b086f59a4d6842df5e31

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713925232418,
+  "lastUpdate": 1713925465531,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -85651,6 +85651,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 68150,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45436839+Cardosaum@users.noreply.github.com",
+            "name": "Matheus Cardoso",
+            "username": "Cardosaum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "90961af6a4b9e19872e1b086f59a4d6842df5e31",
+          "message": "Expand clippy coverage (#1706)\n\nWe're expanding clippy coverage on CI, but currently only for passing\r\ncrates.\r\nA notable one that is still failing is `risc0-r0vm`. This needs to be\r\naddressed in a future PR.\r\n\r\n---------\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>\r\nCo-authored-by: Erik Kaneda <erik@risczero.com>\r\nCo-authored-by: Frank Laub <frank@risczero.com>",
+          "timestamp": "2024-04-24T02:08:07Z",
+          "tree_id": "9f54d957be9c25a1deed5c8cde7d115d76492b7d",
+          "url": "https://github.com/risc0/risc0/commit/90961af6a4b9e19872e1b086f59a4d6842df5e31"
+        },
+        "date": 1713925463324,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 15703716,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 86874,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 77886,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 154315,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 142077,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 85378,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 68380,
             "unit": "Hz"
           }
         ]
