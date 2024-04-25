90cc3e41be5b518f8f48e22ae3d9995b2f5a9597
github-action-benchmark <github@users.noreply.github.com>
2024-04-25 14:44:14 -0700

    add macOS-cpu (customBiggerIsBetter) benchmark result for f7b9f09784615f54467d626674e76aadcd27af48

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714081185221,
+  "lastUpdate": 1714081454259,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -22653,6 +22653,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 16124,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f7b9f09784615f54467d626674e76aadcd27af48",
+          "message": "change `sys_cycle_count` to return `u64` (#1719)\n\nChanging `sys_cycle_count` resulted in various changes to the trait as\r\nwell as several other type changes throughout the system including\r\n`SyscallContext`. I've addressed various other type errors as well as\r\n`TODO(breaking change): update to `u64`` comments. Tests have also been\r\nupdated to reflect this type change.\r\n\r\nCloses: #1673",
+          "timestamp": "2024-04-25T21:02:36Z",
+          "tree_id": "65b8a5df839b6cb459e91d9334a335307f50b2e4",
+          "url": "https://github.com/risc0/risc0/commit/f7b9f09784615f54467d626674e76aadcd27af48"
+        },
+        "date": 1714081452055,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 15772659,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 26906,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 18512,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 32313,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 31553,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 27422,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 16118,
             "unit": "Hz"
           }
         ]
