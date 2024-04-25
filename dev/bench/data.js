0e39e31c573f042e6fb72d2edb22ad4e9cb0bc87
github-action-benchmark <github@users.noreply.github.com>
2024-04-25 20:30:30 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 876dfe3dc73ca94cd38abc27e6e3445524a7f390

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714076981808,
+  "lastUpdate": 1714077029732,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -89618,6 +89618,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 192373,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "45436839+Cardosaum@users.noreply.github.com",
+            "name": "Matheus Cardoso",
+            "username": "Cardosaum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "876dfe3dc73ca94cd38abc27e6e3445524a7f390",
+          "message": "Address clippy errors for `risc0-r0vm` (#1722)\n\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-04-25T20:21:33Z",
+          "tree_id": "1270ff529ebc46d883a56ca11e6baaf1a95b7022",
+          "url": "https://github.com/risc0/risc0/commit/876dfe3dc73ca94cd38abc27e6e3445524a7f390"
+        },
+        "date": 1714077025638,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 10205011,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 234148,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 232382,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 384208,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 300151,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 226990,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 194026,
             "unit": "Hz"
           }
         ]
