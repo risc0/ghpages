eb2b044fd9f94d762a4961b68279501be458dcf0
github-action-benchmark <github@users.noreply.github.com>
2024-04-26 18:36:19 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 11602c70d22e24da197caa28783de937eb5e3676

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714156404953,
+  "lastUpdate": 1714156578538,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -65167,6 +65167,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 19696,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "11602c70d22e24da197caa28783de937eb5e3676",
+          "message": "Cleanups (#1730)\n\n* Bump rust to 1.77\r\n* Drop stale crates\r\n* Drop stale workspace\r\n* Drop stale deps\r\n* Consolidate CI workflows",
+          "timestamp": "2024-04-26T11:24:20-07:00",
+          "tree_id": "7630c08d5208a83f5232cb7e0337fb4d0c9280d4",
+          "url": "https://github.com/risc0/risc0/commit/11602c70d22e24da197caa28783de937eb5e3676"
+        },
+        "date": 1714156573864,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 9446767,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 32922,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 23151,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 42968,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 41553,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 32494,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 20418,
             "unit": "Hz"
           }
         ]
