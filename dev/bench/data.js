bcce09d34b517f147da9fd223540f838fe5b979e
github-action-benchmark <github@users.noreply.github.com>
2024-04-30 19:31:30 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for c37165bf24c4deeb5e7070e4d4e32724038d428e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714442258368,
+  "lastUpdate": 1714505490187,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -66342,6 +66342,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "composite",
             "value": 35162,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 21355,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c37165bf24c4deeb5e7070e4d4e32724038d428e",
+          "message": "Parallelize step_verify_bytes (#1751)",
+          "timestamp": "2024-04-30T19:24:44Z",
+          "tree_id": "4af826669d398017d5d8d6f497ae478e5399c4d1",
+          "url": "https://github.com/risc0/risc0/commit/c37165bf24c4deeb5e7070e4d4e32724038d428e"
+        },
+        "date": 1714505485521,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 9253506,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 35481,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24376,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43772,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42169,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 35269,
             "unit": "Hz"
           },
           {
