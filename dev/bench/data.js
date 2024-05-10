16ab644be9cd448826970840c09970d6da836966
github-action-benchmark <github@users.noreply.github.com>
2024-05-10 20:28:18 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 50df09054b9c32b74853d328fca946c74675cb28

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715370078486,
+  "lastUpdate": 1715372897603,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "macOS-cpu": [
@@ -95636,6 +95636,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 414806,
+            "unit": "Hz"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "104483650+MartinKong1990@users.noreply.github.com",
+            "name": "MartinKong1990",
+            "username": "MartinKong1990"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "50df09054b9c32b74853d328fca946c74675cb28",
+          "message": "Update dev-mode Broken Link (#1808)\n\nOld:\r\nhttps://dev.risczero.com/api/zkvm/dev-mode\r\nhttps://dev.risczero.com/zkvm/dev-mode\r\n\r\nNew:\r\nhttps://dev.risczero.com/api/generating-proofs/dev-mode",
+          "timestamp": "2024-05-10T13:22:42-07:00",
+          "tree_id": "1fc7188e5cbed16256699430fa8390ae7f3cb5ed",
+          "url": "https://github.com/risc0/risc0/commit/50df09054b9c32b74853d328fca946c74675cb28"
+        },
+        "date": 1715372893248,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 11123968,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 562276,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 556245,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 575483,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 427295,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 536061,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 416712,
             "unit": "Hz"
           }
         ]
