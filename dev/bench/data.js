90b2859783b2e257c018d1cf102dfd523f2c3cfa
github-action-benchmark <github@users.noreply.github.com>
2024-05-11 19:22:47 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 93e87e75feaf7a679403ab7cd9c58a22bc2f8e63

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -10901,6 +10901,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "93e87e75feaf7a679403ab7cd9c58a22bc2f8e63",
+          "message": "Update dependencies  (#1824)\n\n* cargo update\r\n* Use xtask bootstrap-protos to pre-compile and commit the protobuf\r\nfiles so that users don't need to have protoc installed (or built from\r\nsource).\r\n* drop some cruft",
+          "timestamp": "2024-05-11T19:15:39-07:00",
+          "tree_id": "6bc3516f712708abc16c33970a963f7c4099ee70",
+          "url": "https://github.com/risc0/risc0/commit/93e87e75feaf7a679403ab7cd9c58a22bc2f8e63"
+        },
+        "date": 1715480565293,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 14040631,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 109712,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 95153,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 147067,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 136985,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 107576,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 81258,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -14558,6 +14617,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1715420087637,
+  "lastUpdate": 1715480567527,
   "repoUrl": "https://github.com/risc0/risc0"
 }
