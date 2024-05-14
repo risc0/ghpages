fa7f6fd1ccee417a878a6d14b6bbd0a251089d4d
github-action-benchmark <github@users.noreply.github.com>
2024-05-14 18:53:29 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 4bab3011da2fc4ed98316b896071b18934358f08

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -3770,6 +3770,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "angelocapossele@gmail.com",
+            "name": "Angelo Capossele",
+            "username": "capossele"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4bab3011da2fc4ed98316b896071b18934358f08",
+          "message": "Add conversions for compact receipt (#1831)\n\n- Add converions for compact receipts so that can be used with `r0vm`\r\n- Make Groth16 docker prover silent, so that can be used with the\r\nrisc0-ethereum `ffi`\r\n- Fix few typos",
+          "timestamp": "2024-05-14T19:41:20+01:00",
+          "tree_id": "4bdb4d45558b2390f8b58b571321cad3b88bffcb",
+          "url": "https://github.com/risc0/risc0/commit/4bab3011da2fc4ed98316b896071b18934358f08"
+        },
+        "date": 1715712803111,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 10498142,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 35618,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24668,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43810,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42414,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 35874,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 21525,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -15207,6 +15266,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1715712626889,
+  "lastUpdate": 1715712808558,
   "repoUrl": "https://github.com/risc0/risc0"
 }
