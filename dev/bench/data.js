0ba23c87cdf4129b4607dfba1a1b91e13038428c
github-action-benchmark <github@users.noreply.github.com>
2024-05-23 11:23:44 -0700

    add macOS-cpu (customBiggerIsBetter) benchmark result for e364553a708c3dd99a5e1c9705111e7baa422e75

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -17505,9 +17505,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e364553a708c3dd99a5e1c9705111e7baa422e75",
+          "message": "Minor docs updates (#1866)\n\n* Drop warnings\r\n* Fix terminology links",
+          "timestamp": "2024-05-23T18:16:40Z",
+          "tree_id": "5d9c5e63095ae3789bd0979cf8348ef6c1dc5fa9",
+          "url": "https://github.com/risc0/risc0/commit/e364553a708c3dd99a5e1c9705111e7baa422e75"
+        },
+        "date": 1716488621928,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 15274266,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 29229,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 18774,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 31803,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 31298,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 27860,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 16260,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1716488596922,
+  "lastUpdate": 1716488623848,
   "repoUrl": "https://github.com/risc0/risc0"
 }
