2b1069a0bbc41f0f34d6e167e40e00adb6d1ffa3
github-action-benchmark <github@users.noreply.github.com>
2024-05-23 17:14:15 -0700

    add macOS-cpu (customBiggerIsBetter) benchmark result for a793ffdfb07d62cc5397edc5f3c97a67e61988a4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -18272,9 +18272,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor@risczero.com",
+            "name": "Victor Graf",
+            "username": "nategraf"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a793ffdfb07d62cc5397edc5f3c97a67e61988a4",
+          "message": "Rename CompactReceipt to Groth16Receipt (#1877)\n\nThis PR renames `CompactReceipt` and associated data types and function\r\nto `Groth16Receipt`. It might help increase discoverability when folks\r\nneed a Groth16 receipt to post on Ethereum and elsewhere.",
+          "timestamp": "2024-05-24T00:07:13Z",
+          "tree_id": "547c0384a7218bc92de0f17ccc85e02a0062314d",
+          "url": "https://github.com/risc0/risc0/commit/a793ffdfb07d62cc5397edc5f3c97a67e61988a4"
+        },
+        "date": 1716509653466,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 14814159,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 29671,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 18680,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 31659,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 31099,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 28594,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 16154,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1716509632553,
+  "lastUpdate": 1716509655309,
   "repoUrl": "https://github.com/risc0/risc0"
 }
