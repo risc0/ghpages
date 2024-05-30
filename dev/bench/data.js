9d464d02bdf769ff214860740c84b6b39cfe8839
github-action-benchmark <github@users.noreply.github.com>
2024-05-30 16:38:08 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for c393f590b68cfd1dd0d8d167b9d6833ef832d428

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -4832,6 +4832,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c393f590b68cfd1dd0d8d167b9d6833ef832d428",
+          "message": "recursion circuit: create an empty zkr.zip file when building docs (#1907)\n\nThis fixes docs.rs build errors that we've been seeing for\r\n`risc0-circuit-recursion` and hopefully `risc0-zkvm`. I was able to test\r\nthis locally and verify that `risc0-circuit-recursion` has been fixed\r\nbut `risc0-zkvm` fetches the published `risc0-circuit-recursion` crate\r\nso changes to not appear in my local testing environment.",
+          "timestamp": "2024-05-30T09:30:02-07:00",
+          "tree_id": "39869d72ba581911da51116bb9da86b263591774",
+          "url": "https://github.com/risc0/risc0/commit/c393f590b68cfd1dd0d8d167b9d6833ef832d428"
+        },
+        "date": 1717087082419,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 9437032,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 35878,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24605,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43696,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42186,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 35918,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 21463,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -19455,6 +19514,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1717087021042,
+  "lastUpdate": 1717087087543,
   "repoUrl": "https://github.com/risc0/risc0"
 }
