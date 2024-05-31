94fc44e74a7b2422afcb820f0f6ff1faa80a0176
github-action-benchmark <github@users.noreply.github.com>
2024-05-30 21:29:05 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for b075247d847893c63f38fe2f8f2c1d837f99d87a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -14677,6 +14677,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tim.zerrell@risczero.com",
+            "name": "Tim Zerrell",
+            "username": "tzerrell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b075247d847893c63f38fe2f8f2c1d837f99d87a",
+          "message": "Increase ZK_CYCLES (#1910)\n\nIncrease the number of cycles used to ensure zero knowledge.",
+          "timestamp": "2024-05-30T21:23:22-07:00",
+          "tree_id": "415ef9afcc52e9eed694bb8355eb1000cfbddfeb",
+          "url": "https://github.com/risc0/risc0/commit/b075247d847893c63f38fe2f8f2c1d837f99d87a"
+        },
+        "date": 1717129743376,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 12894805,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 98819,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 91926,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 151441,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 134555,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 102338,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 79496,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -19573,6 +19632,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1717129667130,
+  "lastUpdate": 1717129745275,
   "repoUrl": "https://github.com/risc0/risc0"
 }
