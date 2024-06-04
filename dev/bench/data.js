47c5596e7b9cc9dce072bffa09df5936717b4759
github-action-benchmark <github@users.noreply.github.com>
2024-06-03 18:12:44 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 661e2fe33f6e16d94c67c926ce36af5e191ef8e3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -15621,6 +15621,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "151764357+polymaer@users.noreply.github.com",
+            "name": "PolyMa",
+            "username": "polymaer"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "661e2fe33f6e16d94c67c926ce36af5e191ef8e3",
+          "message": "fix: remove repetitive words in comments (#1891)\n\nremoved the duplicated words in the comments to improve readability. no\r\nproduction code was touched.",
+          "timestamp": "2024-06-03T17:47:33-07:00",
+          "tree_id": "21cbd1d2569323c998a832bed723a697a86ccb5f",
+          "url": "https://github.com/risc0/risc0/commit/661e2fe33f6e16d94c67c926ce36af5e191ef8e3"
+        },
+        "date": 1717463563007,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 12873182,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 108010,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 92916,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 148783,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 137239,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 105568,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 80018,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -20812,6 +20871,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1717462670731,
+  "lastUpdate": 1717463564713,
   "repoUrl": "https://github.com/risc0/risc0"
 }
