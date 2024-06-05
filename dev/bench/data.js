9226ac0f4d6e71811a7f6345eb035fc0504f483e
github-action-benchmark <github@users.noreply.github.com>
2024-06-05 01:06:40 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for dae7a0667dea7978e37d2fb4c4dac0f8f83d5a39

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -10669,6 +10669,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "dae7a0667dea7978e37d2fb4c4dac0f8f83d5a39",
+          "message": "Hotfix for CUDA build issues (#1929)",
+          "timestamp": "2024-06-05T01:02:15Z",
+          "tree_id": "449b9fda74d40a5790f4d50673095c6e7b15df46",
+          "url": "https://github.com/risc0/risc0/commit/dae7a0667dea7978e37d2fb4c4dac0f8f83d5a39"
+        },
+        "date": 1717549595432,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 10477007,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 559916,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 556573,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 540204,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 389644,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 526397,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 407169,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -21461,6 +21520,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1717549589409,
+  "lastUpdate": 1717549599930,
   "repoUrl": "https://github.com/risc0/risc0"
 }
