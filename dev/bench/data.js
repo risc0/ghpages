261589bf6a9789c58bb0c83ff9ed51271c79e75f
github-action-benchmark <github@users.noreply.github.com>
2024-06-19 15:40:21 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 07e8d682af73bff0905eca31565824d2326200d7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -11613,6 +11613,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "07e8d682af73bff0905eca31565824d2326200d7",
+          "message": "rust toolchain: build cargo with static libraries (#1987)\n\nSetting this option to `true` in the config.toml will build the cargo\r\nutility with static libraries. This avoids the versioning issue that\r\nwe've seen where cargo needs to link with older versions of OpenSSL.\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-06-19T15:35:53Z",
+          "tree_id": "0829a6802025381923f65b21ff3d1107c3a9b0f7",
+          "url": "https://github.com/risc0/risc0/commit/07e8d682af73bff0905eca31565824d2326200d7"
+        },
+        "date": 1718811616415,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21957302,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 571582,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 567669,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 549910,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 401685,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 551570,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 421566,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -23457,6 +23516,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1718651894636,
+  "lastUpdate": 1718811621047,
   "repoUrl": "https://github.com/risc0/risc0"
 }
