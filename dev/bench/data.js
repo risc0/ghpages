548c6744912bea5c0916988f22d872db5644901f
github-action-benchmark <github@users.noreply.github.com>
2024-06-20 18:29:24 -0700

    add macOS-cpu (customBiggerIsBetter) benchmark result for 920932e18f0420b1cb7ceb6f794018b624037eba

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -23700,6 +23700,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tim.zerrell@risczero.com",
+            "name": "Tim Zerrell",
+            "username": "tzerrell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "920932e18f0420b1cb7ceb6f794018b624037eba",
+          "message": "Increase ZK Cycles to 1994 (#1970)\n\nUse 1994 cycles for zk noise. Note that this changes associated tests,\r\nincluding making a split test smaller so it continues to split only once\r\neven with the larger cycle overhead.\r\n\r\n---------\r\n\r\nCo-authored-by: Frank Laub <frank@risczero.com>\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-06-21T01:21:41Z",
+          "tree_id": "dcc02e0edf339eec7d927096339d43cbca296491",
+          "url": "https://github.com/risc0/risc0/commit/920932e18f0420b1cb7ceb6f794018b624037eba"
+        },
+        "date": 1718933362345,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 32144512,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 28215,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 18166,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 31547,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 31010,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 27872,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 16308,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-g6.xlarge": [
@@ -23870,6 +23929,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1718933327132,
+  "lastUpdate": 1718933364248,
   "repoUrl": "https://github.com/risc0/risc0"
 }
