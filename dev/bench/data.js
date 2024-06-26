4c95516ea3e18f6674ea3dd2f0e46f1312a90f18
github-action-benchmark <github@users.noreply.github.com>
2024-06-26 18:30:18 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for e58b7f6b47dbed7206f55bdc0029700f22f4cf18

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -6189,6 +6189,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e58b7f6b47dbed7206f55bdc0029700f22f4cf18",
+          "message": "risc0-build: change `risc0_data` to support revamped `rzup` (#2028)\n\nrzup will store toolchains in the ~/.rzup directory. Check if this new\r\ndirectory does not exist, fallback to the old directory.\r\n\r\ncloses: #2017",
+          "timestamp": "2024-06-26T18:21:02Z",
+          "tree_id": "04495fbd016fed826b4edb81910b836c89649953",
+          "url": "https://github.com/risc0/risc0/commit/e58b7f6b47dbed7206f55bdc0029700f22f4cf18"
+        },
+        "date": 1719426612137,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20203010,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 34852,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24011,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43858,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42327,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 34794,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 20980,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -24932,6 +24991,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1719426387019,
+  "lastUpdate": 1719426617624,
   "repoUrl": "https://github.com/risc0/risc0"
 }
