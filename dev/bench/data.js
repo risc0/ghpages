b9e454c5f65127152408759646d2d7d58ef8df0f
github-action-benchmark <github@users.noreply.github.com>
2024-06-28 10:33:11 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 69df8bb2c288b5006c44927c89234a18850543b6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -19515,6 +19515,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "69df8bb2c288b5006c44927c89234a18850543b6",
+          "message": "serde: serializer: implement collect_str (#2035)\n\nThe panic in this function was causing chrono's `NaiveDate` to fail\r\nserialization. In other words, this prevented users from passing\r\n`NaiveDate` as an input for the guest.",
+          "timestamp": "2024-06-28T10:07:27-07:00",
+          "tree_id": "bf9684eec6adc60a6ddd407c0662cbf74607836a",
+          "url": "https://github.com/risc0/risc0/commit/69df8bb2c288b5006c44927c89234a18850543b6"
+        },
+        "date": 1719595989251,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 33737156,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 71664,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 77344,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 160329,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 142944,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 71787,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 70050,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -26171,6 +26230,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1719595519269,
+  "lastUpdate": 1719595991293,
   "repoUrl": "https://github.com/risc0/risc0"
 }
