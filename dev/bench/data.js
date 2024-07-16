c009da8ad20cb5fc8e6607d9501b4ca7800c1d5f
github-action-benchmark <github@users.noreply.github.com>
2024-07-16 18:55:42 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 76ee0c5f89b570f39bf7b0ea4bed6df9531868bc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -14319,6 +14319,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "76ee0c5f89b570f39bf7b0ea4bed6df9531868bc",
+          "message": "rust-toolchain: remove deprecated `changelog-seen` (#2099)\n\nThis has been removed as of 1.79 and if left in this file, it will break\r\nthe rust toolchain build for 1.79.",
+          "timestamp": "2024-07-16T18:50:44Z",
+          "tree_id": "5e6ad301d48186eb084a0e34c02936db7a506b9d",
+          "url": "https://github.com/risc0/risc0/commit/76ee0c5f89b570f39bf7b0ea4bed6df9531868bc"
+        },
+        "date": 1721156139178,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21634440,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 462804,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 462419,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 482518,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 335805,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 447439,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 349547,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -28869,6 +28928,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1721098320214,
+  "lastUpdate": 1721156142106,
   "repoUrl": "https://github.com/risc0/risc0"
 }
