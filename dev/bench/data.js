0849bd92f13019dc4de7527f20074ad35309f1eb
github-action-benchmark <github@users.noreply.github.com>
2024-07-29 09:59:13 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 5483fb5d4fd5aaffe68970d18eca65827a04f6ed

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -7955,6 +7955,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5483fb5d4fd5aaffe68970d18eca65827a04f6ed",
+          "message": "Only check pointer if length is non-zero. (#2137)\n\nA pointer is allowed to be any undefined value if the original slice's\r\nlength is 0.\r\n\r\nSee: https://github.com/rust-lang/rust/issues/128257",
+          "timestamp": "2024-07-29T09:52:27Z",
+          "tree_id": "fabb9792d7dca463baf19804a536ebb2e92d4a58",
+          "url": "https://github.com/risc0/risc0/commit/5483fb5d4fd5aaffe68970d18eca65827a04f6ed"
+        },
+        "date": 1722247146889,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20908996,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 34966,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24171,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43752,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42333,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 34898,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 21232,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -31406,6 +31465,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1722247033297,
+  "lastUpdate": 1722247152657,
   "repoUrl": "https://github.com/risc0/risc0"
 }
