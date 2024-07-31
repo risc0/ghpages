75975f568c479f2a6e65cc35ffd00b9056cd689a
github-action-benchmark <github@users.noreply.github.com>
2024-07-31 00:35:28 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for bc3730d2a5fb19e62b43d14e0a2300d5f6bc532a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16793,6 +16793,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bc3730d2a5fb19e62b43d14e0a2300d5f6bc532a",
+          "message": "ZKVM-480: Add experimental support for sys_fork (#2084)\n\nCo-authored-by: Nikolai Vazquez <hello@nikolaivazquez.com>",
+          "timestamp": "2024-07-30T17:30:34-07:00",
+          "tree_id": "52ef987bb139ce26fe25f97bebdeac74709701ee",
+          "url": "https://github.com/risc0/risc0/commit/bc3730d2a5fb19e62b43d14e0a2300d5f6bc532a"
+        },
+        "date": 1722386125425,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20405540,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 539619,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 545093,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 543783,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 374761,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 514155,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 401587,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -32873,6 +32932,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1722373434315,
+  "lastUpdate": 1722386128085,
   "repoUrl": "https://github.com/risc0/risc0"
 }
