906a8e2cc2eb0e0543c230ee291cce8113f149aa
github-action-benchmark <github@users.noreply.github.com>
2024-08-01 03:22:25 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 0ff09d94baf7076892df7bea918dd6018ceb2580

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -33165,9 +33165,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0ff09d94baf7076892df7bea918dd6018ceb2580",
+          "message": "CI: replace cargo risczero install with rzup install (#2149)\n\nThis replaces uses of `cargo risczero install` with the new `rzup\r\ninstall rust $toolchain_version`. There are several workloads that\r\nutilize the cargo risczero utility so I left the CI rules for those\r\nintact.\r\n---------\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-07-31T20:10:27-07:00",
+          "tree_id": "a232475d98f4f57490649a1a97482c8ae50e5e66",
+          "url": "https://github.com/risc0/risc0/commit/0ff09d94baf7076892df7bea918dd6018ceb2580"
+        },
+        "date": 1722482538059,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22186772,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 430017,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 445223,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 469107,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 326789,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 424612,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 335071,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1722482120154,
+  "lastUpdate": 1722482545026,
   "repoUrl": "https://github.com/risc0/risc0"
 }
