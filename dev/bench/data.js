861d9f74bbaacaac03e28bd22d262c9ab7757084
github-action-benchmark <github@users.noreply.github.com>
2024-08-05 08:42:44 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for a671a5b6e84e15f1ca8c5ae3d07fe442ee0fdec8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -8897,6 +8897,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41407352+hunjixin@users.noreply.github.com",
+            "name": "Mike",
+            "username": "hunjixin"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a671a5b6e84e15f1ca8c5ae3d07fe442ee0fdec8",
+          "message": "fix:tty log mix cargo and guest log (#1826)\n\nwhen compiling gust code, i find a strange log ```bevy-guest: Starting\r\nbuild for riscv32im-risc0-zkvm-elfmethods(build) ```, after some test\r\nand code dig, i found that this log is mixed by two lines.\r\n```rs\r\n    Building [=======================> ] 251/254: bevy-methods(build)                                                \r\nbevy-guest: Starting build for riscv32im-risc0-zkvm-elf\r\n\r\n//mixed log\r\nbevy-guest: Starting build for riscv32im-risc0-zkvm-elfmethods(build)\r\n```\r\n\r\nso just write new line in tty env\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-08-05T04:29:55-04:00",
+          "tree_id": "f8c6e9d2e958208650c5f9223721344a9eda133c",
+          "url": "https://github.com/risc0/risc0/commit/a671a5b6e84e15f1ca8c5ae3d07fe442ee0fdec8"
+        },
+        "date": 1722847356233,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20043496,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 33428,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 23417,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 41383,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 40080,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 34082,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 20798,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -34820,6 +34879,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1722847282433,
+  "lastUpdate": 1722847364355,
   "repoUrl": "https://github.com/risc0/risc0"
 }
