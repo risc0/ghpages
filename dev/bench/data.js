a4a33eba1617d6650a94a6adc6d6f750313f1675
github-action-benchmark <github@users.noreply.github.com>
2024-08-13 19:51:44 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for e1618ef4da4d2385e3709e00c9eeaa334a784599

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -9664,6 +9664,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e1618ef4da4d2385e3709e00c9eeaa334a784599",
+          "message": "Clippy now works by default (#2210)\n\nAs long as `RISC0_SKIP_BUILD=1` is specified.",
+          "timestamp": "2024-08-13T12:45:43-07:00",
+          "tree_id": "50316da37b84fee659f544d102334267383276f3",
+          "url": "https://github.com/risc0/risc0/commit/e1618ef4da4d2385e3709e00c9eeaa334a784599"
+        },
+        "date": 1723578696312,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22452856,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 35746,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24503,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 44327,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 43168,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 35803,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 21500,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -37888,6 +37947,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1723578676365,
+  "lastUpdate": 1723578704281,
   "repoUrl": "https://github.com/risc0/risc0"
 }
