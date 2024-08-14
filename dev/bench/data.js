080374f0446cf26ef9fa213b4774c3a961077309
github-action-benchmark <github@users.noreply.github.com>
2024-08-14 22:09:44 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for fb1d3836cfdb1f8e634f2394579d16a1cfde4cc5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -9959,6 +9959,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nils@risczero.com",
+            "name": "shkoo",
+            "username": "shkoo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fb1d3836cfdb1f8e634f2394579d16a1cfde4cc5",
+          "message": "Move risc0-circuit-bigint to risc0 repo (#2201)",
+          "timestamp": "2024-08-14T22:03:33Z",
+          "tree_id": "15b730ef81cfe96b34756885f0323f251c8fc4d5",
+          "url": "https://github.com/risc0/risc0/commit/fb1d3836cfdb1f8e634f2394579d16a1cfde4cc5"
+        },
+        "date": 1723673376178,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21617926,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 35311,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24326,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 44144,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42703,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 35309,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 21154,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -38950,6 +39009,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1723673254563,
+  "lastUpdate": 1723673384127,
   "repoUrl": "https://github.com/risc0/risc0"
 }
