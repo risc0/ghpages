28fdcfbaa1359bfb98e1885896ac77c88ec64fd1
github-action-benchmark <github@users.noreply.github.com>
2024-08-23 11:13:44 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for f865f638a02b21408c0ffe41daf0c8e8bab868e9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -32005,6 +32005,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hello@nikolaivazquez.com",
+            "name": "Nikolai Vazquez",
+            "username": "nvzqz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f865f638a02b21408c0ffe41daf0c8e8bab868e9",
+          "message": "Add `seal_size` method to receipt types (#2262)",
+          "timestamp": "2024-08-23T18:08:24Z",
+          "tree_id": "e479aaaf5b6c76ef7a87432ce991229e2107e713",
+          "url": "https://github.com/risc0/risc0/commit/f865f638a02b21408c0ffe41daf0c8e8bab868e9"
+        },
+        "date": 1724436821982,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 32700136,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 113719,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 97029,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 157491,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 146945,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 112164,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 84227,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -41723,6 +41782,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1724436745304,
+  "lastUpdate": 1724436823940,
   "repoUrl": "https://github.com/risc0/risc0"
 }
