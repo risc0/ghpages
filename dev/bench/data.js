f3578078b33c07024886bdba557350bfd48831c0
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 02:59:11 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for fd9449310e9dfef6938f81051becb52f8bfa8502

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -10726,6 +10726,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hello@nikolaivazquez.com",
+            "name": "Nikolai Vazquez",
+            "username": "nvzqz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fd9449310e9dfef6938f81051becb52f8bfa8502",
+          "message": "Add missing `segment_limit_po2` in `cargo risczero datasheet` (#2266)\n\nThis was accidentally not included when converting from the original\r\ndatasheet example code.",
+          "timestamp": "2024-08-27T02:52:51Z",
+          "tree_id": "ca86712d12ef21cb95bdc1b778960a825ddbd7f2",
+          "url": "https://github.com/risc0/risc0/commit/fd9449310e9dfef6938f81051becb52f8bfa8502"
+        },
+        "date": 1724727543215,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22792492,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 35304,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24305,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 44033,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42763,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 35058,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 21324,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -42018,6 +42077,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1724727470958,
+  "lastUpdate": 1724727551161,
   "repoUrl": "https://github.com/risc0/risc0"
 }
