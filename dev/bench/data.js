97e6430f6957485ad8fb809c3a9343a80c68af13
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 00:40:29 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for a03f28d9e402bc54a813f15f04055ba908ca470a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -21749,6 +21749,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "hello@nikolaivazquez.com",
+            "name": "Nikolai Vazquez",
+            "username": "nvzqz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a03f28d9e402bc54a813f15f04055ba908ca470a",
+          "message": "Make `CompositeReceipt::seal_size` consider assumptions (#2264)\n\nWithout assumption receipts, the `CompositeReceipt` is not verifiable.",
+          "timestamp": "2024-08-28T00:36:26Z",
+          "tree_id": "a0e01639fbcacc200e94db71f43c2d4be51d1d7b",
+          "url": "https://github.com/risc0/risc0/commit/a03f28d9e402bc54a813f15f04055ba908ca470a"
+        },
+        "date": 1724805626192,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21589132,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 759301,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 765633,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 539166,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 396727,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 721496,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 522920,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -42608,6 +42667,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1724803195613,
+  "lastUpdate": 1724805628934,
   "repoUrl": "https://github.com/risc0/risc0"
 }
