2ebe62c48a1bc62ce2161b653cdea6b5fce6876c
github-action-benchmark <github@users.noreply.github.com>
2024-09-04 06:50:10 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for c7af1d350844b6014e688e6fbc66bd2520499e40

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -22988,6 +22988,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "2418646+hashcashier@users.noreply.github.com",
+            "name": "Rami",
+            "username": "hashcashier"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c7af1d350844b6014e688e6fbc66bd2520499e40",
+          "message": "fix(risc0-build): Fix null-builds under custom rustc flags (#2301)\n\nThis PR gives each guest its own target directory based on its host\r\npackage and guest package names to fix build-caching when using\r\nnon-uniform rustc flags for different guests under the same host\r\npackage.",
+          "timestamp": "2024-09-04T06:43:14Z",
+          "tree_id": "3755eff41bbc12751aeaeda9867cbc186b93581a",
+          "url": "https://github.com/risc0/risc0/commit/c7af1d350844b6014e688e6fbc66bd2520499e40"
+        },
+        "date": 1725432606917,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21429254,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 756900,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 764497,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 538989,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 396987,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 721142,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 522314,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -45086,6 +45145,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1725432590092,
+  "lastUpdate": 1725432609874,
   "repoUrl": "https://github.com/risc0/risc0"
 }
