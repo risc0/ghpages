c30d071d5d7ce540b585bd7aad03202fa22092aa
github-action-benchmark <github@users.noreply.github.com>
2024-09-10 04:41:27 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 66de2f0473c5117af1ff7e155a7c95bd44451580

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -46086,9 +46086,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "66de2f0473c5117af1ff7e155a7c95bd44451580",
+          "message": "docs and readme: update to 1.1 (#2317)\n\ncreate new versioned dev docs for 1.1\r\nupdate readme to use 1.1 crates.io links\r\nupdate readme to use new `rzup` command\r\nupdate readme to label `metal` feature as deprecated\r\n\r\nThis should be merged after 1.1 has been published.\r\n\r\n---------\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>\r\nCo-authored-by: Frank Laub <frank@risczero.com>",
+          "timestamp": "2024-09-10T03:01:55Z",
+          "tree_id": "d2cb99b6c09194222287de891090b32df4275d77",
+          "url": "https://github.com/risc0/risc0/commit/66de2f0473c5117af1ff7e155a7c95bd44451580"
+        },
+        "date": 1725943279285,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22541984,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 481469,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 484506,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 440988,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 312970,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 467610,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 367170,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1725938160789,
+  "lastUpdate": 1725943287241,
   "repoUrl": "https://github.com/risc0/risc0"
 }
