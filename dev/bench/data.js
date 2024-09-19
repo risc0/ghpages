9616da93dc53ef68f00c88c97e02946540b3f121
github-action-benchmark <github@users.noreply.github.com>
2024-09-19 19:20:34 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 981d80610dc895c387ad46d88f6925ea9a1f62a6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -24345,6 +24345,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "981d80610dc895c387ad46d88f6925ea9a1f62a6",
+          "message": "Support wider range of client/server use cases (#2356)\n\nCo-authored-by: Parker Thompson <parker@risczero.com>",
+          "timestamp": "2024-09-19T12:16:14-07:00",
+          "tree_id": "6b58221072579c45b1cb2ff5e0ba0331f78bb264",
+          "url": "https://github.com/risc0/risc0/commit/981d80610dc895c387ad46d88f6925ea9a1f62a6"
+        },
+        "date": 1726773630682,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21648632,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 755071,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 763515,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 537992,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 394909,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 719958,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 522092,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -47800,6 +47859,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1726638634420,
+  "lastUpdate": 1726773633589,
   "repoUrl": "https://github.com/risc0/risc0"
 }
