c1dda3f81c5dd7f48141d6379ac5608a0f664916
github-action-benchmark <github@users.noreply.github.com>
2024-09-19 20:23:09 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for f3ada1b83422104400235eb3a3b8122820630b6a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -48210,9 +48210,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor@risczero.com",
+            "name": "Victor Graf",
+            "username": "nategraf"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3ada1b83422104400235eb3a3b8122820630b6a",
+          "message": "Fix a typo in the description of `env::verify_intergrity` (#2337)\n\nNoticed a typo and some formatting issues with the docs on\r\n`env::verify_integrity`.",
+          "timestamp": "2024-09-19T20:15:56Z",
+          "tree_id": "310631106213ec527175875067d3bbbaef01a1ea",
+          "url": "https://github.com/risc0/risc0/commit/f3ada1b83422104400235eb3a3b8122820630b6a"
+        },
+        "date": 1726777381407,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22109298,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 487487,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 494829,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 445532,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 314262,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 459660,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 365748,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1726777343039,
+  "lastUpdate": 1726777389436,
   "repoUrl": "https://github.com/risc0/risc0"
 }
