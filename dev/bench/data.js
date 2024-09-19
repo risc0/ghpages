466f284e5eb5416c08e18c8a049c3fcba6e5d2c2
github-action-benchmark <github@users.noreply.github.com>
2024-09-19 19:23:36 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 981d80610dc895c387ad46d88f6925ea9a1f62a6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -47974,9 +47974,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "981d80610dc895c387ad46d88f6925ea9a1f62a6",
+          "message": "Support wider range of client/server use cases (#2356)\n\nCo-authored-by: Parker Thompson <parker@risczero.com>",
+          "timestamp": "2024-09-19T12:16:14-07:00",
+          "tree_id": "6b58221072579c45b1cb2ff5e0ba0331f78bb264",
+          "url": "https://github.com/risc0/risc0/commit/981d80610dc895c387ad46d88f6925ea9a1f62a6"
+        },
+        "date": 1726773807911,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21960964,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 474231,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 489171,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 450373,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 320486,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 472802,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 359845,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1726773800282,
+  "lastUpdate": 1726773815991,
   "repoUrl": "https://github.com/risc0/risc0"
 }
