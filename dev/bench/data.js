21f1a137c4277a309b2f06d51101779446c6ac9d
github-action-benchmark <github@users.noreply.github.com>
2024-10-02 13:28:20 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for aa80206707044a0d54583223f08f16131dd7ee9c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -37846,6 +37846,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nuke-web3@proton.me",
+            "name": "Nuke 🌄",
+            "username": "nuke-web3"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa80206707044a0d54583223f08f16131dd7ee9c",
+          "message": "`bun run fmt-markdown` run on `/website` (#2395)",
+          "timestamp": "2024-10-02T16:23:48-04:00",
+          "tree_id": "3106be3e87d229ad8b9b962863da0940f09cd190",
+          "url": "https://github.com/risc0/risc0/commit/aa80206707044a0d54583223f08f16131dd7ee9c"
+        },
+        "date": 1727900898531,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 32692554,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 114556,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 98461,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 159688,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 147203,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 112791,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 84079,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -49511,6 +49570,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1727900879400,
+  "lastUpdate": 1727900900002,
   "repoUrl": "https://github.com/risc0/risc0"
 }
