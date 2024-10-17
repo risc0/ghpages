32de5c494428d07ce13f941fbae0ca351aa4a37c
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 22:35:44 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 0a52bd0a60bf4c92d67a2d66f99b636feff98866

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -26233,6 +26233,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0a52bd0a60bf4c92d67a2d66f99b636feff98866",
+          "message": "Add input generation benchmarks (#2426)\n\n* Add unconstrained cycle log\r\n* Fix format/clippy issues\r\n* Move tests to module dirs",
+          "timestamp": "2024-10-17T15:31:41-07:00",
+          "tree_id": "07ab25674f686eac138fe122481f350aa14d82fc",
+          "url": "https://github.com/risc0/risc0/commit/0a52bd0a60bf4c92d67a2d66f99b636feff98866"
+        },
+        "date": 1729204541665,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21697280,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 728674,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 734094,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 530390,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 392653,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 694081,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 505488,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -51517,6 +51576,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1729116430392,
+  "lastUpdate": 1729204544042,
   "repoUrl": "https://github.com/risc0/risc0"
 }
