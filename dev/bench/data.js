08edaddaa437fc269574db37c1128bd63249e65c
github-action-benchmark <github@users.noreply.github.com>
2024-11-04 19:45:23 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 29f56fb1c61485d686b89d15e3743f68d67b10f3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -27649,6 +27649,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "29f56fb1c61485d686b89d15e3743f68d67b10f3",
+          "message": "Fix missing cstdint include (#2463)\n\nThis might fix #2449",
+          "timestamp": "2024-11-04T11:41:09-08:00",
+          "tree_id": "c53ded26001581817559d2b02197f141f8786fc4",
+          "url": "https://github.com/risc0/risc0/commit/29f56fb1c61485d686b89d15e3743f68d67b10f3"
+        },
+        "date": 1730749520678,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21768408,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 993419,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 1006949,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 592965,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 418132,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 929056,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 636976,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -54349,6 +54408,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1730306747110,
+  "lastUpdate": 1730749523230,
   "repoUrl": "https://github.com/risc0/risc0"
 }
