a14b07e5cdb5a768550f811268b6002e54b8de00
github-action-benchmark <github@users.noreply.github.com>
2024-11-12 22:32:21 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for d8552d45e4ba6cdd42b7414fb9d175b88f808f8a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -14325,6 +14325,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d8552d45e4ba6cdd42b7414fb9d175b88f808f8a",
+          "message": "CI: fix ethereum workload by pinning syn (#2500)\n\nThis is a workaround for https://github.com/near/borsh-rs/issues/319\r\n\r\n---------\r\n\r\nCo-authored-by: Victor Graf <victor@risczero.com>",
+          "timestamp": "2024-11-12T22:25:01Z",
+          "tree_id": "2b8fc8880906335e857e5a7f3be209be601af9a3",
+          "url": "https://github.com/risc0/risc0/commit/d8552d45e4ba6cdd42b7414fb9d175b88f808f8a"
+        },
+        "date": 1731450723759,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20952208,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 35234,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 24341,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43999,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42793,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 35194,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 21243,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -56355,6 +56414,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1731450726948,
+  "lastUpdate": 1731450741138,
   "repoUrl": "https://github.com/risc0/risc0"
 }
