3cbec0056b15111d79360ed1b8feaa93f431cd6f
github-action-benchmark <github@users.noreply.github.com>
2024-11-13 09:42:23 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 3c20ddcb2f5a707115d1cea896fb375818d798cf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -56824,9 +56824,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nils@risczero.com",
+            "name": "shkoo",
+            "username": "shkoo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3c20ddcb2f5a707115d1cea896fb375818d798cf",
+          "message": "Add sys_prove_keccak system call to invoke the keccak coprocessor callback (#2496)",
+          "timestamp": "2024-11-13T09:34:58Z",
+          "tree_id": "70d87b7e9474502553c96073a4ec5b1fc53918ec",
+          "url": "https://github.com/risc0/risc0/commit/3c20ddcb2f5a707115d1cea896fb375818d798cf"
+        },
+        "date": 1731490935416,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21523928,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 706718,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 730479,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 525116,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 351439,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 675415,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 494638,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1731490896641,
+  "lastUpdate": 1731490943410,
   "repoUrl": "https://github.com/risc0/risc0"
 }
