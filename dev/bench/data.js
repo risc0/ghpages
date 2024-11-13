0a6d9ee8b714ca668a3d2bc9374def73d64bb466
github-action-benchmark <github@users.noreply.github.com>
2024-11-13 20:34:12 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 542d644d277e65576e2e9660b731bbe00cd7d304

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -29065,6 +29065,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "542d644d277e65576e2e9660b731bbe00cd7d304",
+          "message": "Implement plumbing for keccak acceleration (#2503)\n\nThis PR introduces a batching mechanism for keccak circuit proofs and\r\nsupersedes #2380\r\n\r\nResolves ZKVM-595",
+          "timestamp": "2024-11-13T20:29:56Z",
+          "tree_id": "3b468c3269b01626f1b1e69bfeb4d7fce21de011",
+          "url": "https://github.com/risc0/risc0/commit/542d644d277e65576e2e9660b731bbe00cd7d304"
+        },
+        "date": 1731530048958,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21713404,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 934900,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 951156,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 606624,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 425116,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 876744,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 610625,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -57122,6 +57181,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1731524271119,
+  "lastUpdate": 1731530052357,
   "repoUrl": "https://github.com/risc0/risc0"
 }
