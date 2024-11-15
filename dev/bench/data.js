857a0fc0854f0be15ac90d322671fa132c1ff4bf
github-action-benchmark <github@users.noreply.github.com>
2024-11-15 23:32:49 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for f1ecad14b84d92e989103aa3cc61108257703ad5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -58417,9 +58417,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "austinabell8@gmail.com",
+            "name": "Austin Abell",
+            "username": "austinabell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f1ecad14b84d92e989103aa3cc61108257703ad5",
+          "message": "Constrain buffer length for RSA bigint values (#2510)",
+          "timestamp": "2024-11-15T15:25:09-08:00",
+          "tree_id": "a3b066fd153e68789e779a737ac312cb3340ae36",
+          "url": "https://github.com/risc0/risc0/commit/f1ecad14b84d92e989103aa3cc61108257703ad5"
+        },
+        "date": 1731713559344,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 21942512,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 703380,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 726793,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 525265,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 349700,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 671466,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 492546,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1731713524740,
+  "lastUpdate": 1731713568798,
   "repoUrl": "https://github.com/risc0/risc0"
 }
