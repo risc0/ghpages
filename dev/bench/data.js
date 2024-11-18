fd855baa2b2b7535447f0b4999b36b9e0c3c293a
github-action-benchmark <github@users.noreply.github.com>
2024-11-18 19:38:09 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 0f90d30b3f430384ba48a1795371555c28a29b9e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -29773,6 +29773,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tim.zerrell@risczero.com",
+            "name": "Tim Zerrell",
+            "username": "tzerrell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0f90d30b3f430384ba48a1795371555c28a29b9e",
+          "message": "Add EC Mul and Add acceleration (#2513)\n\nAdds elliptic curve acceleration via bigint2 for addition and scalar\r\nmultiplication. This also includes a fix to allow bigint2 codegen with\r\nnegative intermediates.\r\n\r\nNote that current support is only for secp256k1; more generic curve\r\nsupport is future work.\r\n\r\n---------\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-11-18T19:33:57Z",
+          "tree_id": "99571180bc4315d71827b04a913d697ed75df056",
+          "url": "https://github.com/risc0/risc0/commit/0f90d30b3f430384ba48a1795371555c28a29b9e"
+        },
+        "date": 1731958686410,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22293212,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 945625,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 958089,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 602163,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 414340,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 891929,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 614631,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -58538,6 +58597,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1731713636537,
+  "lastUpdate": 1731958688967,
   "repoUrl": "https://github.com/risc0/risc0"
 }
