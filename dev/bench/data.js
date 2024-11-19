d29381305430018b22a3317b73690e659badfa26
github-action-benchmark <github@users.noreply.github.com>
2024-11-19 20:10:24 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for b8295cea44d791430bdba402174c42193c0fb2ba

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -14974,6 +14974,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cohan.carpentier@gmail.com",
+            "name": "Cohan",
+            "username": "nahoc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b8295cea44d791430bdba402174c42193c0fb2ba",
+          "message": "feat(web): next 15, react 19, proper caching (#1940)\n\n* proper redirects in app benchmarks\r\n* proper error handling if something fails, won't crash the whole app\r\n* caching set to 1 minute, uses ISR properly\r\n* lighthouse 100 on all pages\r\n* bump next to v15\r\n* replace chartjs with recharts\r\n* \"Last updated\" tooltip on datasheet page",
+          "timestamp": "2024-11-19T14:59:57-05:00",
+          "tree_id": "5d0811b17619b5a35a44f7e2efaebbe4a02deebd",
+          "url": "https://github.com/risc0/risc0/commit/b8295cea44d791430bdba402174c42193c0fb2ba"
+        },
+        "date": 1732047015966,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20946370,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 32424,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 22319,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43955,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42716,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 32345,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 19846,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -58951,6 +59010,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1732046855376,
+  "lastUpdate": 1732047024118,
   "repoUrl": "https://github.com/risc0/risc0"
 }
