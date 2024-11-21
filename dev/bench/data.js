0bde8aa391430f3cd04f9f200fa5890be34a60c6
github-action-benchmark <github@users.noreply.github.com>
2024-11-21 01:32:47 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 9400e4408416510301d07cc3a2dd6d62611f0941

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -59125,9 +59125,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9400e4408416510301d07cc3a2dd6d62611f0941",
+          "message": "remove pin on syn crate (#2529)\n\nborsh has released v1.5.3 that specifies a minimum version for syn. This\r\nallows us to safely remove the pin from our crates. This PR also updates\r\ndb-dump to fix the crates validator.",
+          "timestamp": "2024-11-21T01:25:45Z",
+          "tree_id": "c94bb272b9eaaa79465846e3c69dffef29baf0f4",
+          "url": "https://github.com/risc0/risc0/commit/9400e4408416510301d07cc3a2dd6d62611f0941"
+        },
+        "date": 1732152758877,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20060328,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 716008,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 733755,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 515161,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 345149,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 675453,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 492411,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1732152612827,
+  "lastUpdate": 1732152767067,
   "repoUrl": "https://github.com/risc0/risc0"
 }
