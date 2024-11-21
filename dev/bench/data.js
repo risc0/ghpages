11ec8ff9d13c53180bb92d009c42c002690f01ee
github-action-benchmark <github@users.noreply.github.com>
2024-11-21 01:36:51 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 9400e4408416510301d07cc3a2dd6d62611f0941

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -15033,6 +15033,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9400e4408416510301d07cc3a2dd6d62611f0941",
+          "message": "remove pin on syn crate (#2529)\n\nborsh has released v1.5.3 that specifies a minimum version for syn. This\r\nallows us to safely remove the pin from our crates. This PR also updates\r\ndb-dump to fix the crates validator.",
+          "timestamp": "2024-11-21T01:25:45Z",
+          "tree_id": "c94bb272b9eaaa79465846e3c69dffef29baf0f4",
+          "url": "https://github.com/risc0/risc0/commit/9400e4408416510301d07cc3a2dd6d62611f0941"
+        },
+        "date": 1732153002913,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20871776,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 31796,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 22130,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 44077,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42593,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 32016,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 19620,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -59187,6 +59246,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1732152767067,
+  "lastUpdate": 1732153011106,
   "repoUrl": "https://github.com/risc0/risc0"
 }
