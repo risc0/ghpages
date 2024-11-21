25bd9082749901fca6d6860bda675fd7fe820178
github-action-benchmark <github@users.noreply.github.com>
2024-11-20 17:30:13 -0800

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 9400e4408416510301d07cc3a2dd6d62611f0941

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -45044,6 +45044,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9400e4408416510301d07cc3a2dd6d62611f0941",
+          "message": "remove pin on syn crate (#2529)\n\nborsh has released v1.5.3 that specifies a minimum version for syn. This\r\nallows us to safely remove the pin from our crates. This PR also updates\r\ndb-dump to fix the crates validator.",
+          "timestamp": "2024-11-21T01:25:45Z",
+          "tree_id": "c94bb272b9eaaa79465846e3c69dffef29baf0f4",
+          "url": "https://github.com/risc0/risc0/commit/9400e4408416510301d07cc3a2dd6d62611f0941"
+        },
+        "date": 1732152611217,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 33090824,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 100607,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 86901,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 160779,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 147584,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 97851,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 75986,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -59069,6 +59128,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1732152597479,
+  "lastUpdate": 1732152612827,
   "repoUrl": "https://github.com/risc0/risc0"
 }
