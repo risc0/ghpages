47f58f8440e123e54829c601d3e87c39e49bd70e
github-action-benchmark <github@users.noreply.github.com>
2024-12-03 15:16:24 -0800

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 533aff23f633c6102c961d47b9370ec96770de6d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -48053,6 +48053,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "austinabell8@gmail.com",
+            "name": "Austin Abell",
+            "username": "austinabell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "533aff23f633c6102c961d47b9370ec96770de6d",
+          "message": "enable unstable feature for cargo risczero test (#2598)\n\nNot adding additional semantics by handling an unstable feature flag,\r\ngiven this is already gated behind `experimental` feature flag.\r\n\r\nNeeded to be able to test new unstable syscalls\r\n\r\n---------\r\n\r\nCo-authored-by: Victor Graf <victor@risczero.com>",
+          "timestamp": "2024-12-03T23:12:01Z",
+          "tree_id": "757e66068f621d8213b42893b3d880854f45a364",
+          "url": "https://github.com/risc0/risc0/commit/533aff23f633c6102c961d47b9370ec96770de6d"
+        },
+        "date": 1733267782063,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 33168126,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 100003,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 87039,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 161697,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 149329,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 98861,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 75555,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -63081,6 +63140,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1733267771286,
+  "lastUpdate": 1733267783826,
   "repoUrl": "https://github.com/risc0/risc0"
 }
