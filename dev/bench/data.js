fc8c2eb85eba514361a285047cb44111baa7beb6
github-action-benchmark <github@users.noreply.github.com>
2024-12-06 23:22:53 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for f5b919fbe9d1fe47054f6868bae47f87398fc33e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -32487,6 +32487,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nils@risczero.com",
+            "name": "shkoo",
+            "username": "shkoo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f5b919fbe9d1fe47054f6868bae47f87398fc33e",
+          "message": "Programmatically split keccak funcs, move to consistent namespaces (#2616)\n\n* cuda is now in risc0::circuit::keccak::cuda, c++ is now in\r\nrisc0::circuit::keccak::cpu\r\n* Split execution generation into multiple files for parallel\r\ncompilation\r\n* Bump KECCAK_PO2 down to 16, since 17 is right on the edge of memory\r\nexhaustion",
+          "timestamp": "2024-12-06T15:18:44-08:00",
+          "tree_id": "98478b151e9aef6f55be78b60fe70ca36d1ef57b",
+          "url": "https://github.com/risc0/risc0/commit/f5b919fbe9d1fe47054f6868bae47f87398fc33e"
+        },
+        "date": 1733527370110,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22129624,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 938383,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 950915,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 622478,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 432955,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 890203,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 620176,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -63966,6 +64025,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1733480478203,
+  "lastUpdate": 1733527372798,
   "repoUrl": "https://github.com/risc0/risc0"
 }
