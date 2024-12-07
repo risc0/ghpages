7d70f9af4a47bf799c324b0940e755293da02dea
github-action-benchmark <github@users.noreply.github.com>
2024-12-07 00:41:14 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for d02b9c62e49497a7d328e65afb4639a881c8243d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -32605,6 +32605,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jeremy@risczero.com",
+            "name": "Jeremy Bruestle",
+            "username": "jbruestle"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d02b9c62e49497a7d328e65afb4639a881c8243d",
+          "message": "Fix a bug in ConstExt (#2621)\n\nCurrent version has a bug where by reinterpreting u32s, they end up in\r\nMontgomery form: but actually they are coming for the user in normal\r\nform.\r\n\r\n---------\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-12-07T00:37:05Z",
+          "tree_id": "7de4359a69f0fd7c82b8fa4b6cdcf7902d7de831",
+          "url": "https://github.com/risc0/risc0/commit/d02b9c62e49497a7d328e65afb4639a881c8243d"
+        },
+        "date": 1733532071227,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22074746,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 938851,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 951792,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 614719,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 431558,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 878763,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 616138,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -64202,6 +64261,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1733527543726,
+  "lastUpdate": 1733532073810,
   "repoUrl": "https://github.com/risc0/risc0"
 }
