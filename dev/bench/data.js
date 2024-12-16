944f7ec768344ed21916b11b21b7cc1dbd2c05ae
github-action-benchmark <github@users.noreply.github.com>
2024-12-16 23:44:22 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for f74920596c745a8bb7e8a6f1dfdefb44d03ed06f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -34316,6 +34316,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f74920596c745a8bb7e8a6f1dfdefb44d03ed06f",
+          "message": "zkvm: enable configurable po2 for keccak circuit and fix batcher input (#2643)",
+          "timestamp": "2024-12-16T23:40:10Z",
+          "tree_id": "8452ddf0a93cfd93ccc7104e78fd5681f5e3eb2a",
+          "url": "https://github.com/risc0/risc0/commit/f74920596c745a8bb7e8a6f1dfdefb44d03ed06f"
+        },
+        "date": 1734392659754,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22630454,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 989328,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 991897,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 632147,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 435212,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 777849,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 371872,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -67565,6 +67624,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1734120514247,
+  "lastUpdate": 1734392662444,
   "repoUrl": "https://github.com/risc0/risc0"
 }
