d888b71d33bd6a0665fab111dd2f6b3a29b7ac55
github-action-benchmark <github@users.noreply.github.com>
2024-12-17 17:49:17 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 8801e2e3cd030acea2f03f01bd91f37e47e79e23

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -17275,6 +17275,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8801e2e3cd030acea2f03f01bd91f37e47e79e23",
+          "message": "keccak: fix range check to include po2 = 18 and add control ID and update control root (#2660)\n\nThe range of allowed po2's did not include po2 = 18. This PR fixes the\r\nrange check and enables po2 = 18 for downstream performance testing.\r\n\r\n---------\r\n\r\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2024-12-17T17:41:46Z",
+          "tree_id": "438d07b15c7331b67fab94db616dee553e48d1b7",
+          "url": "https://github.com/risc0/risc0/commit/8801e2e3cd030acea2f03f01bd91f37e47e79e23"
+        },
+        "date": 1734457746124,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20901810,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 30264,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 22041,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 44677,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 43348,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 28988,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 15626,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -68155,6 +68214,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1734457586121,
+  "lastUpdate": 1734457756972,
   "repoUrl": "https://github.com/risc0/risc0"
 }
