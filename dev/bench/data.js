0b179a360d743b99d4c1b4a9802df79469c2f7bc
github-action-benchmark <github@users.noreply.github.com>
2025-01-16 21:42:53 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 3fb0cb5bf63e11463fd7f8badcfd800f2aabdb0a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -71456,9 +71456,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "austinabell8@gmail.com",
+            "name": "Austin Abell",
+            "username": "austinabell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3fb0cb5bf63e11463fd7f8badcfd800f2aabdb0a",
+          "message": "ZKVM-958: Add p256 curve to ecdsa examples (#2710)\n\nKept the example as close to k256 as possible for simplicity, and just\r\nadapted the readme to reflect updated details.\r\n\r\nCloses ZKVM-958\r\n\r\n---------\r\n\r\nCo-authored-by: Tim Zerrell <tim.zerrell@risczero.com>",
+          "timestamp": "2025-01-16T13:35:17-08:00",
+          "tree_id": "eda074450645d7eb293520a6dd10a9be6a0dfe89",
+          "url": "https://github.com/risc0/risc0/commit/3fb0cb5bf63e11463fd7f8badcfd800f2aabdb0a"
+        },
+        "date": 1737063763125,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22576208,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 743126,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 763290,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 526463,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 354745,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 608037,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 304762,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1737063608052,
+  "lastUpdate": 1737063772818,
   "repoUrl": "https://github.com/risc0/risc0"
 }
