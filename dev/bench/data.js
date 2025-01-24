f66a4b9ed1ad083c02eb8a550a7a7c3f7ee8c41b
github-action-benchmark <github@users.noreply.github.com>
2025-01-24 20:50:52 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 7f9870b8dd243bf36357659c015e0679b15bf658

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -18514,6 +18514,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tim.zerrell@risczero.com",
+            "name": "Tim Zerrell",
+            "username": "tzerrell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7f9870b8dd243bf36357659c015e0679b15bf658",
+          "message": "ZKVM-1000: Fix bigint field tests (#2763)",
+          "timestamp": "2025-01-24T12:42:40-08:00",
+          "tree_id": "9be498b94f4759307de4f319bdb96af4d715ed55",
+          "url": "https://github.com/risc0/risc0/commit/7f9870b8dd243bf36357659c015e0679b15bf658"
+        },
+        "date": 1737751840763,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20704910,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 30449,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 21990,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 45052,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 43560,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 28929,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 15662,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -73111,6 +73170,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1737751652216,
+  "lastUpdate": 1737751851892,
   "repoUrl": "https://github.com/risc0/risc0"
 }
