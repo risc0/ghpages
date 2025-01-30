2cf5e5ae8230b798473324291c834cd0fc3fd787
github-action-benchmark <github@users.noreply.github.com>
2025-01-30 02:35:03 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 5a7fa0389b974479b9bfc2355265d61527ed9f25

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -75055,9 +75055,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bobbobbio@gmail.com",
+            "name": "Remi Bernotavicius",
+            "username": "bobbobbio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5a7fa0389b974479b9bfc2355265d61527ed9f25",
+          "message": "[AUD-7] Fix heap-pointer overflow in sys_alloc_aligned (#2778)",
+          "timestamp": "2025-01-29T18:22:40-08:00",
+          "tree_id": "2b1208e24b35568f39908651f6b24413b9077f0b",
+          "url": "https://github.com/risc0/risc0/commit/5a7fa0389b974479b9bfc2355265d61527ed9f25"
+        },
+        "date": 1738204493120,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20509096,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 738653,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 761961,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 534875,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 360276,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 601366,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 304857,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1738204248194,
+  "lastUpdate": 1738204502684,
   "repoUrl": "https://github.com/risc0/risc0"
 }
