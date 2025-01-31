5e9c8faf6e331948a6f0bd657ac0e4fd75cff00b
github-action-benchmark <github@users.noreply.github.com>
2025-01-31 00:01:21 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 11624074763e4202ee658f942895057db597bcc6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -75232,9 +75232,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "victor@risczero.com",
+            "name": "Victor Snyder-Graf",
+            "username": "nategraf"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "11624074763e4202ee658f942895057db597bcc6",
+          "message": "ZKVM-1011: Add unstable feature flag to risc0-zkp Cargo.toml to address warning and allow usage of unstable feature (#2779)\n\nhttps://github.com/risc0/risc0/pull/2658 introduced some API surface\nunder `#[stability::unstable]` but forgot to add the `unstable` flag. I\nsaw warnings about this in the CI builds and so figured I should open\nthis PR\n\nCo-authored-by: Frank Laub <flaub@risc0.com>",
+          "timestamp": "2025-01-30T15:53:38-08:00",
+          "tree_id": "d204d7207dc6913118ab8613e36ca3a477d66ff7",
+          "url": "https://github.com/risc0/risc0/commit/11624074763e4202ee658f942895057db597bcc6"
+        },
+        "date": 1738281671071,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20467400,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 733337,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 755915,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 533200,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 359927,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 598406,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 303446,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1738281514809,
+  "lastUpdate": 1738281681266,
   "repoUrl": "https://github.com/risc0/risc0"
 }
