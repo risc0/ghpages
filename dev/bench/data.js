00832e59860214a9443025541ea30a44f32723ad
github-action-benchmark <github@users.noreply.github.com>
2025-02-14 18:17:48 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 06d7b296fcc1d3618c3a1ac01df269d31812d07c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -81840,9 +81840,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "06d7b296fcc1d3618c3a1ac01df269d31812d07c",
+          "message": "ZKVM-1091: Cleanup parallel union support (#2833)",
+          "timestamp": "2025-02-14T10:08:55-08:00",
+          "tree_id": "99ca62b1b62fa98719699e089ff9d75010cc9d50",
+          "url": "https://github.com/risc0/risc0/commit/06d7b296fcc1d3618c3a1ac01df269d31812d07c"
+        },
+        "date": 1739557057380,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20291634,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 729056,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 750251,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 539618,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 361761,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 598244,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 303840,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1739556847843,
+  "lastUpdate": 1739557068318,
   "repoUrl": "https://github.com/risc0/risc0"
 }
