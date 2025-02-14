33161357577aa8e441a253435d9defe1f401f9e3
github-action-benchmark <github@users.noreply.github.com>
2025-02-14 18:19:40 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 06d7b296fcc1d3618c3a1ac01df269d31812d07c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -20697,6 +20697,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "06d7b296fcc1d3618c3a1ac01df269d31812d07c",
+          "message": "ZKVM-1091: Cleanup parallel union support (#2833)",
+          "timestamp": "2025-02-14T10:08:55-08:00",
+          "tree_id": "99ca62b1b62fa98719699e089ff9d75010cc9d50",
+          "url": "https://github.com/risc0/risc0/commit/06d7b296fcc1d3618c3a1ac01df269d31812d07c"
+        },
+        "date": 1739557169716,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 19283150,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 29530,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 21347,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43314,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 41981,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 28024,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 15157,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -81902,6 +81961,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1739557068318,
+  "lastUpdate": 1739557180083,
   "repoUrl": "https://github.com/risc0/risc0"
 }
