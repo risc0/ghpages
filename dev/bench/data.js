4dfa2410f623614e0533760a82b1f7e90ddbeadf
github-action-benchmark <github@users.noreply.github.com>
2025-02-19 04:05:20 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 711b163551c96ccfbfc8271b7634d945997ff457

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -20815,6 +20815,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bobbobbio@gmail.com",
+            "name": "Remi Bernotavicius",
+            "username": "bobbobbio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "711b163551c96ccfbfc8271b7634d945997ff457",
+          "message": "ZKVM-1105: Don't read past end of bigintBytes buffer in v2 CUDA kernel (#2850)",
+          "timestamp": "2025-02-18T19:50:55-08:00",
+          "tree_id": "5867e79d2127c35299f786cbe39817192edc2e93",
+          "url": "https://github.com/risc0/risc0/commit/711b163551c96ccfbfc8271b7634d945997ff457"
+        },
+        "date": 1739937909060,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 19405406,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 29495,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 21297,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43452,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42108,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 27929,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 15088,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -82374,6 +82433,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1739937668132,
+  "lastUpdate": 1739937919946,
   "repoUrl": "https://github.com/risc0/risc0"
 }
