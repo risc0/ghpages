c0305fec2fd039d6272198e817d539815246a4d5
github-action-benchmark <github@users.noreply.github.com>
2025-02-22 09:44:19 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 968a5a3ed5cb23a72a09e31b666d4ab74c07d36a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -42104,6 +42104,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "968a5a3ed5cb23a72a09e31b666d4ab74c07d36a",
+          "message": "ZKVM-1124: fix benchmarks (#2865)\n\nThe v2 executor incurs 1 less cycle. I found through testing that each\niteration adds 2 cycles so I've lowered the loop count and expected\ncycle count by 1 to stay under the M1 cycle threshold",
+          "timestamp": "2025-02-22T09:34:09Z",
+          "tree_id": "e69b9b43a3a99767c19c5d343cb398e53e717fd5",
+          "url": "https://github.com/risc0/risc0/commit/968a5a3ed5cb23a72a09e31b666d4ab74c07d36a"
+        },
+        "date": 1740217456016,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 20581892,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 825129,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 826326,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 643803,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 433223,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 779482,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 576381,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -83259,6 +83318,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1740217448550,
+  "lastUpdate": 1740217459136,
   "repoUrl": "https://github.com/risc0/risc0"
 }
