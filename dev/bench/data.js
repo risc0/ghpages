3d9aa74204d7e0730aa16c4df16f1ed48fe990d7
github-action-benchmark <github@users.noreply.github.com>
2025-02-25 23:12:33 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for c042488d967a47c084aca65f11cf925dd265198f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -83905,9 +83905,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c042488d967a47c084aca65f11cf925dd265198f",
+          "message": "ZKVM-1147: compat test: fix compat test by adding `--locked` to install command (#2885)",
+          "timestamp": "2025-02-25T15:04:30-08:00",
+          "tree_id": "a20dc66dc5c29310b6b77eb47d0f55023e1f1c90",
+          "url": "https://github.com/risc0/risc0/commit/c042488d967a47c084aca65f11cf925dd265198f"
+        },
+        "date": 1740525142171,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 22834782,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 545726,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 543880,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 564872,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 360366,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 519976,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 404794,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1740524937320,
+  "lastUpdate": 1740525153148,
   "repoUrl": "https://github.com/risc0/risc0"
 }
