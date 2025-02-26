9e4626288f24d6c2a52b86ed76d0c7e4c2088845
github-action-benchmark <github@users.noreply.github.com>
2025-02-26 09:50:54 -0800

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 1ba7b8eb21a9ae9af977f2c71afb7c51a0207ed8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -64219,6 +64219,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1ba7b8eb21a9ae9af977f2c71afb7c51a0207ed8",
+          "message": "ZKVM-1146: bump version to 1.4.0-alpha.1 (#2884)\n\nwe also discussed bumping to 2.0, so this could be 2.0.0-alpha.1 as well\n\n---------\n\nCo-authored-by: Remi Bernotavicius <remi@abort.cc>",
+          "timestamp": "2025-02-26T17:41:18Z",
+          "tree_id": "421450b0205737d38b60f2ae893e995c3bd2f7e2",
+          "url": "https://github.com/risc0/risc0/commit/1ba7b8eb21a9ae9af977f2c71afb7c51a0207ed8"
+        },
+        "date": 1740592249752,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 33125770,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 14014,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 13621,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 163357,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 150921,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 13844,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 13211,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -84616,6 +84675,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1740591945069,
+  "lastUpdate": 1740592254050,
   "repoUrl": "https://github.com/risc0/risc0"
 }
