a533f0ef8e9f4d5024f28a8f8cf639c8082cddab
github-action-benchmark <github@users.noreply.github.com>
2025-03-05 23:19:16 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for c6a157b76933908b14886bd1d9f45feabacb703c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -43402,6 +43402,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6a157b76933908b14886bd1d9f45feabacb703c",
+          "message": "ZKVM-1180: Combine user ELF and kernel ELF at build time (#2922)",
+          "timestamp": "2025-03-05T23:14:51Z",
+          "tree_id": "c27eb74e96431468a8300bbb1095438f4cd8336e",
+          "url": "https://github.com/risc0/risc0/commit/c6a157b76933908b14886bd1d9f45feabacb703c"
+        },
+        "date": 1741216753122,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 26709840,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 848605,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 848008,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 644849,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 430974,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 786772,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 582184,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -85737,6 +85796,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1741195602973,
+  "lastUpdate": 1741216756367,
   "repoUrl": "https://github.com/risc0/risc0"
 }
