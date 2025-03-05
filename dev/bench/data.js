7a7ae93d0dd8d196843f17176c55fac04c022cdb
github-action-benchmark <github@users.noreply.github.com>
2025-03-05 23:23:19 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for c6a157b76933908b14886bd1d9f45feabacb703c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -85793,9 +85793,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c6a157b76933908b14886bd1d9f45feabacb703c",
+          "message": "ZKVM-1180: Combine user ELF and kernel ELF at build time (#2922)",
+          "timestamp": "2025-03-05T23:14:51Z",
+          "tree_id": "c27eb74e96431468a8300bbb1095438f4cd8336e",
+          "url": "https://github.com/risc0/risc0/commit/c6a157b76933908b14886bd1d9f45feabacb703c"
+        },
+        "date": 1741216988104,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 31552636,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 552252,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 549372,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 568338,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 361425,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 530382,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 415583,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1741216756367,
+  "lastUpdate": 1741216999275,
   "repoUrl": "https://github.com/risc0/risc0"
 }
