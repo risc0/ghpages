95336747487fcad675701c584fa68e62fa3b65de
github-action-benchmark <github@users.noreply.github.com>
2025-03-08 02:38:56 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for b7452d13ced33ca2316f717f5b0bafcca2b7bddc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -43520,6 +43520,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bobbobbio@gmail.com",
+            "name": "Remi Bernotavicius",
+            "username": "bobbobbio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b7452d13ced33ca2316f717f5b0bafcca2b7bddc",
+          "message": "ZKVM-1193: Apply some fixes to Cargo.toml files for next release (#2938)\n\nThese are changes from the release branch that I had to do to publish\nthings. We should commit them to main so we don't have to repeat them\nfor the next release.",
+          "timestamp": "2025-03-08T02:34:33Z",
+          "tree_id": "7677639df78a02d810da24cde477ad0ec076a254",
+          "url": "https://github.com/risc0/risc0/commit/b7452d13ced33ca2316f717f5b0bafcca2b7bddc"
+        },
+        "date": 1741401532161,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 26602898,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 851290,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 850451,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 645609,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 430658,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 800943,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 577187,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -85973,6 +86032,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1741217090337,
+  "lastUpdate": 1741401535842,
   "repoUrl": "https://github.com/risc0/risc0"
 }
