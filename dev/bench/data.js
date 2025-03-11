71f888ce64a2239284c53d4b120136651983925a
github-action-benchmark <github@users.noreply.github.com>
2025-03-11 17:25:51 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 3699ed151e5e0786de7cb67bc1b78b158f8e66da

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -86560,9 +86560,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3699ed151e5e0786de7cb67bc1b78b158f8e66da",
+          "message": "ZKVM-1202: Replace risc0-circuit-rv32im with risc0-circuit-rv32im-v2 (#2952)",
+          "timestamp": "2025-03-11T10:16:34-07:00",
+          "tree_id": "26874172c228001f1e4b92c7c52dbf1b26b38dcf",
+          "url": "https://github.com/risc0/risc0/commit/3699ed151e5e0786de7cb67bc1b78b158f8e66da"
+        },
+        "date": 1741713938846,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 29676604,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 545860,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 543870,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 567673,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 361278,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 525842,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 412632,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1741713934291,
+  "lastUpdate": 1741713951192,
   "repoUrl": "https://github.com/risc0/risc0"
 }
