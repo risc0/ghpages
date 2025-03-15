1adbc897fc8867d786b72959d4b7f64d782074e0
github-action-benchmark <github@users.noreply.github.com>
2025-03-15 00:48:21 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 1d511c1d0a23c647cda25b987bcfe15ba23c0002

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -45054,6 +45054,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d511c1d0a23c647cda25b987bcfe15ba23c0002",
+          "message": "ZKVM-1230: Add version to rv32im seal (#2978)",
+          "timestamp": "2025-03-15T00:43:57Z",
+          "tree_id": "792eb017efe882dbf09173b4194b79c23a9487a4",
+          "url": "https://github.com/risc0/risc0/commit/1d511c1d0a23c647cda25b987bcfe15ba23c0002"
+        },
+        "date": 1741999698176,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 26845818,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 849053,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 849007,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 644436,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 428895,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 787504,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 579725,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -89041,6 +89100,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1741991948752,
+  "lastUpdate": 1741999701323,
   "repoUrl": "https://github.com/risc0/risc0"
 }
