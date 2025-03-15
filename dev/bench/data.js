7ab1660b5587a9eec1a0c17e4ca96f4b5aa36132
github-action-benchmark <github@users.noreply.github.com>
2025-03-15 00:52:17 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 1d511c1d0a23c647cda25b987bcfe15ba23c0002

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -89097,9 +89097,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1d511c1d0a23c647cda25b987bcfe15ba23c0002",
+          "message": "ZKVM-1230: Add version to rv32im seal (#2978)",
+          "timestamp": "2025-03-15T00:43:57Z",
+          "tree_id": "792eb017efe882dbf09173b4194b79c23a9487a4",
+          "url": "https://github.com/risc0/risc0/commit/1d511c1d0a23c647cda25b987bcfe15ba23c0002"
+        },
+        "date": 1741999923191,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 30940786,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 552437,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 549575,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 567616,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 359493,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 527676,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 412802,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1741999701323,
+  "lastUpdate": 1741999936513,
   "repoUrl": "https://github.com/risc0/risc0"
 }
