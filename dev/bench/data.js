d6738c8b26e0f08378d98f61ca2607cb45961509
github-action-benchmark <github@users.noreply.github.com>
2025-03-17 20:41:45 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 57f2a6c319ef41de9e92fd17984656552f9ad80b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -89569,9 +89569,68 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tim.zerrell@risczero.com",
+            "name": "Tim Zerrell",
+            "username": "tzerrell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "57f2a6c319ef41de9e92fd17984656552f9ad80b",
+          "message": "ZKVM-1229: Add unchecked/checked versions of ext field ops (#2977)\n\nSplit the extension field precompile operations into checked and\nunchecked versions that do or don't (respectively) check whether any\ncomponent of the result is >= the base field prime.\n\nThe unchecked version is still constrained to give a result equivalent\nto the checked result modulo the prime modulus; the check is just to\nensure that the result is a representation of the field element\n\"normalized\" to its unique value `[0, p)`.\n\n---------\n\nCo-authored-by: iddo <iddo@risczero.com>",
+          "timestamp": "2025-03-17T20:33:57Z",
+          "tree_id": "79e9308d935f27548645ff7a891b28aa12cbd50f",
+          "url": "https://github.com/risc0/risc0/commit/57f2a6c319ef41de9e92fd17984656552f9ad80b"
+        },
+        "date": 1742244094978,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 30843722,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 551303,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 549290,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 577545,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 368712,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 527115,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 413457,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1742243902958,
+  "lastUpdate": 1742244104885,
   "repoUrl": "https://github.com/risc0/risc0"
 }
