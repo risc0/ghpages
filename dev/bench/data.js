ee4e8e59288860d852f50a5d90abd345b8398d62
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 18:20:16 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for e6a275ce3a5428d3e9c68370eb44b586045341fa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -22939,6 +22939,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e6a275ce3a5428d3e9c68370eb44b586045341fa",
+          "message": "ZKVM-1241: Support for dumping and replaying segments (#2990)\n\n* During execution, if `RISC0_DUMP_PATH` is set and an error occurs,\nserialize the current segment and write it to the specified path.\n* `r0vm --segment` can be used to replay this segment later, possibly\nwith extra debug tracing.",
+          "timestamp": "2025-03-19T18:07:31Z",
+          "tree_id": "81e0c0f9af09fba24cae1b334654d14c26257296",
+          "url": "https://github.com/risc0/risc0/commit/e6a275ce3a5428d3e9c68370eb44b586045341fa"
+        },
+        "date": 1742408404948,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 27873714,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 15838,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 15883,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43603,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42454,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 15802,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 14528,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -90870,6 +90929,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1742408383908,
+  "lastUpdate": 1742408415890,
   "repoUrl": "https://github.com/risc0/risc0"
 }
