bff7c8caaa925269327d4b965cc1236cd21e8588
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 20:19:02 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for bf1de2d0f5252f0592d76e74b3304e6054c16834

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -46116,6 +46116,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bobbobbio@gmail.com",
+            "name": "Remi Bernotavicius",
+            "username": "bobbobbio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bf1de2d0f5252f0592d76e74b3304e6054c16834",
+          "message": "ZKVM-1243: Update to Rust 1.85.0 (#2994)\n\nSince this change includes a change to `rzup` since it was released, we\nmust bump its version number.",
+          "timestamp": "2025-03-19T20:14:38Z",
+          "tree_id": "1a5dc11f08209cce8d95d00e275d6c2b4a509865",
+          "url": "https://github.com/risc0/risc0/commit/bf1de2d0f5252f0592d76e74b3304e6054c16834"
+        },
+        "date": 1742415539147,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 26768816,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 849573,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 847960,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 643635,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 431923,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 788511,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 580416,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -91165,6 +91224,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1742412707345,
+  "lastUpdate": 1742415542558,
   "repoUrl": "https://github.com/risc0/risc0"
 }
