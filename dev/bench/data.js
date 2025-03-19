3c041901df47473595baed449a52ba30d9ade0f9
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 20:23:29 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for bf1de2d0f5252f0592d76e74b3304e6054c16834

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -23057,6 +23057,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bobbobbio@gmail.com",
+            "name": "Remi Bernotavicius",
+            "username": "bobbobbio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bf1de2d0f5252f0592d76e74b3304e6054c16834",
+          "message": "ZKVM-1243: Update to Rust 1.85.0 (#2994)\n\nSince this change includes a change to `rzup` since it was released, we\nmust bump its version number.",
+          "timestamp": "2025-03-19T20:14:38Z",
+          "tree_id": "1a5dc11f08209cce8d95d00e275d6c2b4a509865",
+          "url": "https://github.com/risc0/risc0/commit/bf1de2d0f5252f0592d76e74b3304e6054c16834"
+        },
+        "date": 1742415797995,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 27390342,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 15776,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 15779,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43534,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42254,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 15795,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 14461,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -91224,6 +91283,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1742415542558,
+  "lastUpdate": 1742415808879,
   "repoUrl": "https://github.com/risc0/risc0"
 }
