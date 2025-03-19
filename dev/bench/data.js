923053c8d876819083478bb4bfab7a2b0efe68c6
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 12:31:47 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 61cfc742b8e9d794867cee6d994ea0a5d8f3cd14

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -69057,6 +69057,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "61cfc742b8e9d794867cee6d994ea0a5d8f3cd14",
+          "message": "ZKVM-1242: Fix for bigint v1compat to avoid exhausting kernel stack (#2993)\n\nSave/restore the stack pointer when calling `ecall_bigint_v1compat`.",
+          "timestamp": "2025-03-19T19:21:32Z",
+          "tree_id": "3250e00f95fc82f7ed478c601444b7d9e0b8c694",
+          "url": "https://github.com/risc0/risc0/commit/61cfc742b8e9d794867cee6d994ea0a5d8f3cd14"
+        },
+        "date": 1742412705187,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 50090096,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 13845,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 13822,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 164330,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 149946,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 13866,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 13485,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -91106,6 +91165,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1742412692331,
+  "lastUpdate": 1742412707345,
   "repoUrl": "https://github.com/risc0/risc0"
 }
