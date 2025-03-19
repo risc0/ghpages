9f71276173b0e85b53df32618527a3c95a399a01
github-action-benchmark <github@users.noreply.github.com>
2025-03-19 11:19:44 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for e6a275ce3a5428d3e9c68370eb44b586045341fa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -68821,6 +68821,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e6a275ce3a5428d3e9c68370eb44b586045341fa",
+          "message": "ZKVM-1241: Support for dumping and replaying segments (#2990)\n\n* During execution, if `RISC0_DUMP_PATH` is set and an error occurs,\nserialize the current segment and write it to the specified path.\n* `r0vm --segment` can be used to replay this segment later, possibly\nwith extra debug tracing.",
+          "timestamp": "2025-03-19T18:07:31Z",
+          "tree_id": "81e0c0f9af09fba24cae1b334654d14c26257296",
+          "url": "https://github.com/risc0/risc0/commit/e6a275ce3a5428d3e9c68370eb44b586045341fa"
+        },
+        "date": 1742408381415,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 50339108,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 14054,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 14001,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 164026,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 149891,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 14269,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 13498,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -90811,6 +90870,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1742408147188,
+  "lastUpdate": 1742408383908,
   "repoUrl": "https://github.com/risc0/risc0"
 }
