ebcf998ba87271d7089de9f9bcc08fc65084606c
github-action-benchmark <github@users.noreply.github.com>
2025-03-20 20:39:16 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 249d514a45be69fa23577d59d4ad377cb563d257

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -46470,6 +46470,65 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tim.zerrell@risczero.com",
+            "name": "Tim Zerrell",
+            "username": "tzerrell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "249d514a45be69fa23577d59d4ad377cb563d257",
+          "message": "ZKVM-1252: Rename bigint field unchecked & FFI ops (#3003)\n\nRedo the naming conventions for `risc0_bigint2::field` operations. This\nleaves the \"`risc0_bigint2::field::modadd_256`\"-type operations with\ntheir existing names, but pulls the \"unchecked\" operations into an\n`unchecked` submodule, and renames the FFI module from `ffi_field` to\nplain `ffi`.\n\nThis also removes `pub` from the `ffi` module.",
+          "timestamp": "2025-03-20T20:34:52Z",
+          "tree_id": "64ae7086ae85f891bb9a9c6f65955475d4f71d24",
+          "url": "https://github.com/risc0/risc0/commit/249d514a45be69fa23577d59d4ad377cb563d257"
+        },
+        "date": 1742503153152,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 26986858,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/sha-256",
+            "value": 841428,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 840749,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 646701,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 432588,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 788662,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 576885,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -91873,6 +91932,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1742494354835,
+  "lastUpdate": 1742503156512,
   "repoUrl": "https://github.com/risc0/risc0"
 }
