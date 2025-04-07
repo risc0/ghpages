ec4e3bff55e785d5df2776edf4931b6fcb8ba270
github-action-benchmark <github@users.noreply.github.com>
2025-04-07 21:21:09 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for c48844f36688f89274f4a094e519b0dbe061a267

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -95227,9 +95227,63 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tim.zerrell@risczero.com",
+            "name": "Tim Zerrell",
+            "username": "tzerrell"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c48844f36688f89274f4a094e519b0dbe061a267",
+          "message": "ZKVM-1311: Split RSA precompile into multiple modmuls (#3075)\n\nInstead of running a single monolithic RSA precompile, split it into its\nconstituent modmuls.",
+          "timestamp": "2025-04-07T20:39:45Z",
+          "tree_id": "ba0e11a0689ef2c7f66099bfadadda21c9d44d5d",
+          "url": "https://github.com/risc0/risc0/commit/c48844f36688f89274f4a094e519b0dbe061a267"
+        },
+        "date": 1744060859228,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 31250856,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 539887,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 563081,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 361026,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 519921,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 404471,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1744060686422,
+  "lastUpdate": 1744060869034,
   "repoUrl": "https://github.com/risc0/risc0"
 }
