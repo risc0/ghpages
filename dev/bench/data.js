4f674817ec752e6c448b4a5825706f46166eaf91
github-action-benchmark <github@users.noreply.github.com>
2025-04-08 00:09:12 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for 3dabdc1f4ad46ab54d022f6337aa203b43397302

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -24158,6 +24158,60 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "112983328+mars-risc0@users.noreply.github.com",
+            "name": "Mars Saxman",
+            "username": "mars-risc0"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "3dabdc1f4ad46ab54d022f6337aa203b43397302",
+          "message": "ZKVM-1304: Run prover on Mac OS / aarch64 (#3066)",
+          "timestamp": "2025-04-07T23:34:54Z",
+          "tree_id": "970cbfdba4d78414a392c6ecdaba525a6cb2af49",
+          "url": "https://github.com/risc0/risc0/commit/3dabdc1f4ad46ab54d022f6337aa203b43397302"
+        },
+        "date": 1744070928956,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 27580300,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 15882,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43689,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 42403,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 15871,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 14505,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -95500,6 +95554,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1744070928017,
+  "lastUpdate": 1744070951518,
   "repoUrl": "https://github.com/risc0/risc0"
 }
