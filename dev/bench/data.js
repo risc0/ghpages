78107992d6f9a0884813902df36df448ffd61b8f
github-action-benchmark <github@users.noreply.github.com>
2025-04-11 17:27:48 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for f3871b0f8e71e1043fd39c28aab4802c2afef071

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -48416,6 +48416,60 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bobbobbio@gmail.com",
+            "name": "Remi Bernotavicius",
+            "username": "bobbobbio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "f3871b0f8e71e1043fd39c28aab4802c2afef071",
+          "message": "ZKVM-1326: Don't error in the profiler when it gets confused about the call stack (#3088)\n\nIts a bug that the profiler is getting confused about the call stack,\nand as a result may be producing bad samples (although probably just\ncall-stacks which are parented incorrectly)\n\nIts better to not error and produce some kind of profile, even if its\nless-than-perfect in some ways.",
+          "timestamp": "2025-04-11T16:33:30Z",
+          "tree_id": "73af0ef28f0f6c9933d38c75c135bac08fb4424e",
+          "url": "https://github.com/risc0/risc0/commit/f3871b0f8e71e1043fd39c28aab4802c2afef071"
+        },
+        "date": 1744392463381,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 27142540,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 833211,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 646602,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 433489,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 785665,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 581362,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -95770,6 +95824,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1744391731399,
+  "lastUpdate": 1744392467860,
   "repoUrl": "https://github.com/risc0/risc0"
 }
