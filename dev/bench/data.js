0c5405a015d8f531eb0fd27140b76a12381aa574
github-action-benchmark <github@users.noreply.github.com>
2025-04-19 02:00:25 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 4ac16240c8e76583ee6a68a3b71a21c82f37313c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -49064,6 +49064,60 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "erik@risczero.com",
+            "name": "Erik Kaneda",
+            "username": "SchmErik"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4ac16240c8e76583ee6a68a3b71a21c82f37313c",
+          "message": "ZKVM-1339: bump getrandom to 0.3 (#3106)\n\nsupersedes https://github.com/risc0/risc0/pull/3095\nFixes https://github.com/risc0/risc0/issues/3094\n\n---------\n\nCo-authored-by: Jakub Konka <jakub@vlayer.xyz>\nCo-authored-by: bobbobbio <bobbobbio@gmail.com>",
+          "timestamp": "2025-04-19T01:10:38Z",
+          "tree_id": "3e016cb5d209ce5c1484b943f560ecb9b75fb76c",
+          "url": "https://github.com/risc0/risc0/commit/4ac16240c8e76583ee6a68a3b71a21c82f37313c"
+        },
+        "date": 1745028021607,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 27341952,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 838708,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 646443,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 434207,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 788389,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 581302,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -97066,6 +97120,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1745006784549,
+  "lastUpdate": 1745028024933,
   "repoUrl": "https://github.com/risc0/risc0"
 }
