bf84b83864b3c44d407016edc5577f49bbf62491
github-action-benchmark <github@users.noreply.github.com>
2025-04-23 10:28:35 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 67577e8b8f6ded8252f3c7c6ae25ca15faf2abee

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745429304528,
+  "lastUpdate": 1745429314862,
   "repoUrl": "https://github.com/risc0/risc0",
   "entries": {
     "Linux-nvidia_rtx_3090_ti": [
@@ -105,6 +105,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "succinct",
             "value": 14399,
+            "unit": "Hz"
+          }
+        ]
+      }
+    ],
+    "macOS-apple_m2_pro": [
+      {
+        "commit": {
+          "author": {
+            "name": "Mars Saxman",
+            "username": "mars-risc0",
+            "email": "112983328+mars-risc0@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "67577e8b8f6ded8252f3c7c6ae25ca15faf2abee",
+          "message": "ZKVM-1345: Load word-aligned regions by words, not bytes (#3115)\n\nThe base addr and size parameters of a `load_region` call are very often\nword-aligned, so we can load more efficiently word-wise than with a\nbyte-by-byte loop. This improves performance on some bigint-heavy\nworkloads: datasheet exec time for `zeth_shapella_50` goes from 757ms to\n413ms, with smaller improvements seen on the other segments.",
+          "timestamp": "2025-04-22T17:29:00Z",
+          "url": "https://github.com/risc0/risc0/commit/67577e8b8f6ded8252f3c7c6ae25ca15faf2abee"
+        },
+        "date": 1745429311739,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 45259200,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 13659,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 166609,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 150999,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 13874,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 13587,
             "unit": "Hz"
           }
         ]
