d274a9780ec9e8b9b3c9fb1fe865abca2f36be10
github-action-benchmark <github@users.noreply.github.com>
2025-04-23 19:19:50 +0000

    add Linux-nvidia_rtx_3090_ti (customBiggerIsBetter) benchmark result for 67577e8b8f6ded8252f3c7c6ae25ca15faf2abee

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -49276,6 +49276,58 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Mars Saxman",
+            "username": "mars-risc0",
+            "email": "112983328+mars-risc0@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "67577e8b8f6ded8252f3c7c6ae25ca15faf2abee",
+          "message": "ZKVM-1345: Load word-aligned regions by words, not bytes (#3115)\n\nThe base addr and size parameters of a `load_region` call are very often\nword-aligned, so we can load more efficiently word-wise than with a\nbyte-by-byte loop. This improves performance on some bigint-heavy\nworkloads: datasheet exec time for `zeth_shapella_50` goes from 757ms to\n413ms, with smaller improvements seen on the other segments.",
+          "timestamp": "2025-04-22T17:29:00Z",
+          "url": "https://github.com/risc0/risc0/commit/67577e8b8f6ded8252f3c7c6ae25ca15faf2abee"
+        },
+        "date": 1745435987489,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 26736222,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 838515,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 641030,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 432629,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 793284,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 577136,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-apple_m2_pro": [
@@ -97490,6 +97542,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1745435607204,
+  "lastUpdate": 1745435990208,
   "repoUrl": "https://github.com/risc0/risc0"
 }
