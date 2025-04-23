e4a972002dda602a052d7ef57430397068a589f2
github-action-benchmark <github@users.noreply.github.com>
2025-04-23 19:23:15 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for 67577e8b8f6ded8252f3c7c6ae25ca15faf2abee

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -97539,9 +97539,61 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Mars Saxman",
+            "username": "mars-risc0",
+            "email": "112983328+mars-risc0@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "67577e8b8f6ded8252f3c7c6ae25ca15faf2abee",
+          "message": "ZKVM-1345: Load word-aligned regions by words, not bytes (#3115)\n\nThe base addr and size parameters of a `load_region` call are very often\nword-aligned, so we can load more efficiently word-wise than with a\nbyte-by-byte loop. This improves performance on some bigint-heavy\nworkloads: datasheet exec time for `zeth_shapella_50` goes from 757ms to\n413ms, with smaller improvements seen on the other segments.",
+          "timestamp": "2025-04-22T17:29:00Z",
+          "url": "https://github.com/risc0/risc0/commit/67577e8b8f6ded8252f3c7c6ae25ca15faf2abee"
+        },
+        "date": 1745436183804,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 31470314,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 534805,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 566468,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 362381,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 514712,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 401576,
+            "unit": "Hz"
+          }
+        ]
       }
     ]
   },
-  "lastUpdate": 1745435990208,
+  "lastUpdate": 1745436195109,
   "repoUrl": "https://github.com/risc0/risc0"
 }
