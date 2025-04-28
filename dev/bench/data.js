5f1d5f8f35620e75a3008b3510b8f4f56d6943a8
github-action-benchmark <github@users.noreply.github.com>
2025-04-28 18:47:21 +0000

    add Linux-cpu (customBiggerIsBetter) benchmark result for aa88574ffc1b35dd3893f6035f1d132ae78d6aa6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -24910,6 +24910,60 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "flaub@risc0.com",
+            "name": "Frank Laub",
+            "username": "flaub"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa88574ffc1b35dd3893f6035f1d132ae78d6aa6",
+          "message": "ZKVM-1361: Integrate community contributions (#3126)\n\nSource:\n\n* https://github.com/risc0/risc0/pull/3093\n* https://github.com/risc0/risc0/pull/3099\n\n---------\n\nCo-authored-by: FT <140458077+zeevick10@users.noreply.github.com>\nCo-authored-by: Dmytrol <46675332+Dimitrolito@users.noreply.github.com>",
+          "timestamp": "2025-04-28T17:49:32Z",
+          "tree_id": "a62360f4cf4fdde5c782b230e5321dbf174deb6f",
+          "url": "https://github.com/risc0/risc0/commit/aa88574ffc1b35dd3893f6035f1d132ae78d6aa6"
+        },
+        "date": 1745866030434,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 28751066,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 15451,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 43794,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 41290,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 15703,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 14389,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_3090_ti": [
@@ -98247,6 +98301,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1745865825308,
+  "lastUpdate": 1745866041423,
   "repoUrl": "https://github.com/risc0/risc0"
 }
