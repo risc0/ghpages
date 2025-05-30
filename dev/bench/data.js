007a0e6431046156b2260df766d718132222f32e
github-action-benchmark <github@users.noreply.github.com>
2025-05-30 21:27:15 +0000

    add Linux-g6.xlarge (customBiggerIsBetter) benchmark result for fbe1d0bb75c21fe36cefd87bae25f424b711b291

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -101106,6 +101106,60 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bobbobbio@gmail.com",
+            "name": "Remi Bernotavicius",
+            "username": "bobbobbio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fbe1d0bb75c21fe36cefd87bae25f424b711b291",
+          "message": "ZKVM-1410: Add metrics/logging to telemetry (#3198)\n\nThis is a rebased and merged with RPC system version of\nhttps://github.com/risc0/risc0/pull/3191\n\n---------\n\nCo-authored-by: Frank Laub <frank@risczero.com>",
+          "timestamp": "2025-05-30T20:14:48Z",
+          "tree_id": "5048eb019a377746c48f6c7ba43c939eb7119852",
+          "url": "https://github.com/risc0/risc0/commit/fbe1d0bb75c21fe36cefd87bae25f424b711b291"
+        },
+        "date": 1748640423767,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 65635916,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 548544,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 987841,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 764577,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 528451,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 457260,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "Linux-nvidia_rtx_4090": [
@@ -101381,6 +101435,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1748640158339,
+  "lastUpdate": 1748640435029,
   "repoUrl": "https://github.com/risc0/risc0"
 }
