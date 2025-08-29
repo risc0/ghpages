8e47751caabbf374a8e73224cf36879fb976cad0
github-action-benchmark <github@users.noreply.github.com>
2025-08-29 03:14:02 -0700

    add macOS-apple_m2_pro (customBiggerIsBetter) benchmark result for 85a20e89127d41a4fc58ac1bfac0c2d035700bd3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -79151,6 +79151,60 @@ window.BENCHMARK_DATA = {
             "unit": "Hz"
           }
         ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "140454972+sashaodessa@users.noreply.github.com",
+            "name": "sashaodessa",
+            "username": "sashaodessa"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": false,
+          "id": "85a20e89127d41a4fc58ac1bfac0c2d035700bd3",
+          "message": "docs: fix typos in log-builder tests (#3371)\n\nThis PR corrects minor documentation issues in `log-builder.rs`:  \n- Fixed spelling error (`persitent` → `persistent`).  \n- Fixed spelling error (`guarenteed` → `guaranteed`).",
+          "timestamp": "2025-08-29T08:48:45Z",
+          "tree_id": "321661014ffcb048d88e73fe6d3e668ef4035044",
+          "url": "https://github.com/risc0/risc0/commit/85a20e89127d41a4fc58ac1bfac0c2d035700bd3"
+        },
+        "date": 1756462440022,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "execute",
+            "value": 85708280,
+            "unit": "Hz"
+          },
+          {
+            "name": "prove/poseidon2",
+            "value": 13793,
+            "unit": "Hz"
+          },
+          {
+            "name": "lift",
+            "value": 31787,
+            "unit": "Hz"
+          },
+          {
+            "name": "join",
+            "value": 32232,
+            "unit": "Hz"
+          },
+          {
+            "name": "composite",
+            "value": 13570,
+            "unit": "Hz"
+          },
+          {
+            "name": "succinct",
+            "value": 10466,
+            "unit": "Hz"
+          }
+        ]
       }
     ],
     "macOS-cpu": [
@@ -107429,6 +107483,6 @@ window.BENCHMARK_DATA = {
       }
     ]
   },
-  "lastUpdate": 1756462054109,
+  "lastUpdate": 1756462442267,
   "repoUrl": "https://github.com/risc0/risc0"
 }
